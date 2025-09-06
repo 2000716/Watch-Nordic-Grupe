bceab7b9065e065b89806d27ee2fec1f13bf3161
2000716 <SveinOlavHaukaasArstein@hotmail.com>
2025-09-06 11:28:16 +0200

    Add files via upload

---
+++ Filmdata.js
@@ -0,0 +1,196 @@
+const filmer = {
+  "corpse-bride": {
+    tittel: "Corpse Bride",
+    bakgrunn: "https://beam-images.warnermediacdn.com/BEAM_LWM_DELIVERABLES/d28771bd-73ed-49a7-9259-bdb33a115cbc/c628867cbdcd9987018dd7e994bde435f4725dac.jpg?host=wbd-images.prod-vod.h264.io&partner=beamcom",
+    logo: "https://occ-0-8407-92.1.nflxso.net/dnm/api/v6/S4oi7EPZbv2UEPaukW54OORa0S8/AAAABXsHIUIEsoNXp18ZFMeNZGV2VDqmkxZieG_LfgHTMtvUNwYCxJXmI4SMgxIdomfNtMLREHe7vqwoceGjEqzTsD-aHT8veWO5GKQ.png?r=e74",
+    poster: "https://is1-ssl.mzstatic.com/image/thumb/Video124/v4/4b/f6/66/4bf66634-eace-f237-8991-a383189e2b03/pr_source.lsr/392x220.webp",
+    posterVertikal: "https://image.tmdb.org/t/p/w780/isb2Qow76GpqYmsSyfdMfsYAjts.jpg", // portrett
+    beskrivelse: "På tampen av bryllupet sitt gifter den nervøse brudgommen Victor seg ved et uhell med likbruden Emily og stiger ned med henne til underverdenen.",
+    metadata: ["USA", "2005", "9+", "Fantasi / Grøsser", "1 t 13 min"],
+    imdb: "https://www.imdb.com/title/tt0121164/?ref_=nv_sr_srsg_1_tt_5_nm_3_in_0_q_corpse%2520",
+    skuespillere: "Johnny Depp, Helena Bonham Carter, Emily Watson",
+    regissor: "Tim Burton",
+    watchUrl: "film-mal.html?id=corpse-bride"
+  },
+
+  "hook": {
+    tittel: "Hook",
+    bakgrunn: "https://d32qys9a6wm9no.cloudfront.net/images/movies/backdrop/ae/3d838f72e7bea39f327766bd55957a15_1280x720.jpg?t=1636109773",
+    logo: "https://static.wixstatic.com/media/fd2fb2_dac88f2b81fb43858c47fc29a2fe3952~mv2.png",
+    poster: "https://static.wixstatic.com/media/fd2fb2_3b5f5cd114b746e8b05881724a22a0e5~mv2.jpg",
+    posterVertikal: "https://image.tmdb.org/t/p/original/a8xMPQUw8h7E3bJ9Dw596SD8p3v.jpg", // portrett
+    beskrivelse: "Den middelaldrende advokaten Peter Banning må overbevises om at han en gang var den legendariske Peter Pan for å kunne redde barna fra Kaptein Krok.",
+    metadata: ["USA", "1991", "13+", "Fantasi / Familie", "2 t 16 min"],
+    imdb: "https://www.imdb.com/title/tt0102057/?ref_=nv_sr_srsg_1_tt_5_nm_2_in_0_q_HOOK",
+    skuespillere: "Dustin Hoffman, Robin Williams, Julia Roberts",
+    regissor: "Steven Spielberg",
+    watchUrl: "film-mal.html?id=hook"
+  },
+
+  "kaptein-sabeltann": {
+    tittel: "Kaptein Sabeltann",
+    bakgrunn: "https://iptvimagecache-playplus-prod-vip.han.telia.se/original/no/imgv3/GnP7NDuB0Rrw7IqKaTNuGddHmo9uH98LTuGRs8nsXJvIiNzbUb2LoKtBRiiR2Yg-OfX6odiC2nMHfm6XZuDZz-iivlYEPLXGpx3QZMpE21N2HaSNGxmn_HIyz30.jpeg",
+    logo: "https://static.wixstatic.com/media/fd2fb2_38bcd3561623467f8a05f8fb23488188~mv2.png",
+    poster: "https://beam-images.warnermediacdn.com/BEAM_LWM_DELIVERABLES/500cfa5c-264b-4366-9264-342b1569605e/32dcbd1e-b23c-4acd-ba97-2e6b6a42a620?host=wbd-images.prod-vod.h264.io&partner=beamcom&w=500",
+    posterVertikal: "https://i-viaplay-com.akamaized.net/viaplay-prod/316/184/1574841515-a8796d95201d56c1f26a231e3cad4ddd89e2bb01.jpg?width=400&height=600", // portrett
+    beskrivelse: "Legenden sier at Den Magiske Diamant oppfyller ønsker ved fullmåne. Men det er vesle Marco som stikker av med diamanten!",
+    metadata: ["Norge", "2019", "Alle", "Familie / Barn", "1 t 19 min"],
+    imdb: "https://www.imdb.com/title/tt8048384/?ref_=nv_sr_srsg_0_tt_8_nm_0_in_0_q_kaptein%2520sabeltann%2520og%2520den%2520",
+    skuespillere: "Kyrre Haugen Sydness, Siri Skjeggedal, Leonard Valestrand Eike",
+    regissor: "Marit Moum Aune, Rasmus A. Sivertsen",
+    watchUrl: "film-mal.html?id=kaptein-sabeltann"
+  },
+
+  "burlesque": {
+    tittel: "Burlesque",
+    bakgrunn: "https://occ-0-8407-2218.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABTbfoDZNprg4kKg_Azptknl68ULWGPAhGrKE9Z-7ZFiyg57lAt1CHnWYz8e8cWjI-oKpRnNMfMBKb7bFRi6mBtAdnqQ3rNanOmDC.jpg?r=210",
+    logo: "https://upload.wikimedia.org/wikipedia/fr/d/d0/Burlesque_%28film%2C_2010%29_Logo.png",
+    poster: "https://static.wixstatic.com/media/fd2fb2_c85b71dfd8bc4cc6ae7ac39b8513370d~mv2.jpg",
+    posterVertikal: "https://m.media-amazon.com/images/I/81qjANQ8iPL._UF894,1000_QL80_.jpg", // portrett
+    beskrivelse: "Etter å ha dratt fra Iowa med stjerner i øynene, kommer Ali til Los Angeles og nattklubben Burlesque med en drøm om å ta klubben med storm med sopranstemmen.",
+    metadata: ["USA", "2010", "8+", "Musikal", "1 t 59 min"],
+    imdb: "https://www.imdb.com/title/tt1126591/?ref_=nv_sr_srsg_0_tt_8_nm_0_in_0_q_burlesque",
+    skuespillere: "Cher, Christina Aguilera, Alan Cumming",
+    regissor: "Steve Antin",
+    watchUrl: "film-mal.html?id=burlesque"
+  },
+
+  "dog-man": {
+    tittel: "Dog Man",
+    bakgrunn: "https://andersonvision.com/wp-content/uploads/2025/03/Dog-Man-blu-ray-7-scaled.jpg",
+    logo: "https://www.universalpictures.co.uk/tl_files/content/movies/dog_man/title.png",
+    poster: "https://m.media-amazon.com/images/S/pv-target-images/44839e4a60bb8205258ba5fb43d1369c8a3cdd27f628692f6d0bd5c6c0c35ca6.jpg",
+    posterVertikal: "https://i-viaplay-com.akamaized.net/viaplay-prod/849/40/1740747124-2e6f862a9638dd9cefc70ddf014a388f6f2ada76.jpg?width=400&height=600", // portrett
+    beskrivelse: "Etter at en politimann og politihunden hans smelter sammen for å bli Hundemannen, må han stoppe skurken katten Petey.",
+    metadata: ["USA", "2025", "7+", "Barn / Familie", "1 t 29 min"],
+    imdb: "https://www.imdb.com/title/tt10954718/?ref_=nv_sr_srsg_1_tt_6_nm_1_in_0_q_dOg%2520man",
+    skuespillere: "Pete Davidson, Poppy Liu, Lil Rel Howery",
+    regissor: "Peter Hastings",
+    watchUrl: "film-mal.html?id=dog-man"
+  },
+
+  "dyrene-i-hakkabakkeskogen": {
+    tittel: "Dyrene i Hakkebakkeskogen",
+    bakgrunn: "https://images.squarespace-cdn.com/content/v1/582b59736b8f5b751a0cf841/1483043813486-L79UYGV3MT4F5744E5JV/image-asset.jpeg",
+    logo: "https://is1-ssl.mzstatic.com/image/thumb/Features113/v4/15/0e/02/150e0271-d537-03c1-fcce-c94654692013/hqd8703496463884450421.png/760x286.png",
+    poster: "https://is1-ssl.mzstatic.com/image/thumb/Video113/v4/b1/96/ac/b196acbf-3010-b760-3193-702c340000f0/1181928424-WW-AMP_SF.lsr/1200x675.jpg",
+    posterVertikal: "https://i-viaplay-com.akamaized.net/viaplay-prod/894/612/1494855562-3d4fd9d992512643629a5255922564376c705923.jpg?width=400&height=600", // portrett
+    beskrivelse: "I en liten skog bor Klatremus og mange andre dyr. Her lever de livet sitt og synger og danser når de har lyst. Men de må passe på for reven.",
+    metadata: ["Norge", "2016", "Alle", "Barn / Familie", "1 t 15 min"],
+    imdb: "https://www.imdb.com/title/tt4730838/?ref_=nv_sr_srsg_0_tt_7_nm_1_in_0_q_dyrene%2520i%2520",
+    skuespillere: "Espen Bråten Kristoffersen, Stig Henrik Hoff, Wenche Myhre",
+    regissor: "Rasmus A. Sivertsen",
+    watchUrl: "film-mal.html?id=dyrene-i-hakkabakkeskogen"
+  },
+
+  "jul-i-flåklypa": {
+    tittel: "Jul i Flåklypa",
+    bakgrunn: "https://occ-0-8407-2218.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABf_jXS7fg-ggm0v_ygwinuf9ZAf46TiEnc5yHc9if-TcxcEsEh_RIDErLYQpvhnosZF6f3oXPzEdY7xdP0XOSM2xcPDK0s6Ix0HM.jpg?r=9e5",
+    logo: "https://occ-0-3668-38.1.nflxso.net/dnm/api/v6/S4oi7EPZbv2UEPaukW54OORa0S8/AAAABW5AdcvFX4G8XnB_b3uQRtZgvyU3QTyH4uSjNdDIANcjK_MlWXdMffH6J7JaKlt3Xj4_YKWusqEWZ9Xo1CkqN2_OYV51oVqvhw.webp?r=97e",
+    poster: "https://sumo.cdn.tv2.no/imageapi/v3/img/5dbc0a4f498e1dc3eb8394b3-1700644038168",
+    posterVertikal: "https://i-viaplay-com.akamaized.net/viaplay-prod/908/1012/1460156915-30c59a4bbbfa5b5e2995b36a67da4d63eb0efb42.jpg?width=400&height=600", // portrett
+    beskrivelse: "En desperat avisredaktør med planer om å sette en ny snørekord, får tak i en kraftig snøkanon, alt for å prøve å lage store avisoverskrifter før jul.",
+    metadata: ["Norge", "2013", "7+", "Barn / Familie", "1 t 17 min"],
+    imdb: "https://www.imdb.com/title/tt2769896/?ref_=nv_sr_srsg_0_tt_2_nm_0_in_0_q_JUl%2520i%2520fl%25C3%25A5klypa",
+    skuespillere: "Trond Brænne, Kari Ann Grønsund, Trond Høvik, Kåre Conradi, John F. Brungot",
+    regissor: "Rasmus A. Sivertsen",
+    watchUrl: "film-mal.html?id=jul-i-flåklypa"
+  },
+
+  "bamse-og-dunderklokken": {
+    tittel: "Bamse og dunderklokken",
+    bakgrunn: "https://i-viaplay-com.akamaized.net/viaplay-prod/503/868/1569942509-dee645d2f8fc945e16ce8f8610b343b412028df7.jpg?width=1600&height=900",
+    logo: "https://static.wixstatic.com/media/fd2fb2_64dece4f7cc34318a5b4c1da21b57b9d~mv2.png",
+    poster: "https://beam-images.warnermediacdn.com/BEAM_LWM_DELIVERABLES/dbc86275-72ef-4012-a80e-4851224eb8bf/b00246ed-8e34-466b-84cd-340c02e2c6b1?host=wbd-images.prod-vod.h264.io&partner=beamcom&w=500",
+    posterVertikal: "https://i-viaplay-com.akamaized.net/viaplay-prod/503/868/1571304540-fe2ce9da3baf8ce50054b7f3dca79517f4b77fc2.jpg?width=400&height=600", // portrett
+    beskrivelse: "Lageret av ingrediensen som gjør Bamse til verdens sterkeste bjørn blir ødelagt i en brann, og når Bamse ikke lenger kan bli sterk, er gode råd dyre!",
+    metadata: ["Danmark", "2018", "Alle", "Barn / Familie", "1 t 1 min"],
+    imdb: "https://www.imdb.com/title/tt9472442/?ref_=nv_sr_srsg_0_tt_1_nm_0_in_0_q_bamse%2520og%2520dunder",
+    skuespillere: "Dennis Storhøi, Morten Røhrt, Jan Martin Johnsen, Morten Rudå",
+    regissor: "Christian Ryltenius",
+    watchUrl: "film-mal.html?id=bamse-og-dunderklokken"
+  },
+
+  "elio": {
+    tittel: "Elio",
+    bakgrunn: "https://4kwallpapers.com/images/wallpapers/elio-2024-movies-3840x2160-11787.jpg",
+    logo: "https://disney.images.edge.bamgrid.com/ripcut-delivery/v2/variant/disney/0ab02b15-ed86-4b64-bf03-c8f5a96c493c/compose?format=webp",
+    poster: "https://disney.images.edge.bamgrid.com/ripcut-delivery/v2/variant/disney/ea82917b-65b0-4634-8943-6f13d5a8b368/compose?aspectRatio=1.78&format=webp&width=1200",
+    posterVertikal: "https://i-viaplay-com.akamaized.net/viaplay-prod/245/364/1752570888-782417530bfe7ecef61002484c7fa89e6b91b514.jpg?width=400&height=600", // portrett
+    beskrivelse: "En romfanatiker med en aktiv fantasi, befinner seg på et kosmisk uhell der han må knytte nye bånd med utenomjordiske livsformer.",
+    metadata: ["USA", "2025", "7+", "Familie", "1 t 39 min"],
+    imdb: "https://www.imdb.com/title/tt4900148/?ref_=nv_sr_srsg_0_tt_2_nm_6_in_0_q_Elio",
+    skuespillere: "Yonas Kibreab, Zoe Saldaña, Remy Edgerly",
+    regissor: "Adrian Molina, Domee Shi, Madeline Sharafian",
+    watchUrl: "film-mal.html?id=elio"
+  },
+
+  "elvis": {
+    tittel: "Elvis",
+    bakgrunn: "https://beam-images.warnermediacdn.com/BEAM_LWM_DELIVERABLES/70b04aa5-5a83-4236-8212-a8fedd40a909/ffb7c539224df8be6babe981435998cba6a26af6.jpg?host=wbd-images.prod-vod.h264.io&partner=beamcom",
+    logo: "https://images.cdn.prd.api.discomax.com/rJRVh/3YrKVjDLFbjKTSAzA.png?w=600&f=webp",
+    poster: "https://m.media-amazon.com/images/M/MV5BMTk0ODgyOTk0MV5BMl5BanBnXkFtZTgwMzY0MTI4NjE@._V1_FMjpg_UX1000_.jpg",
+    posterVertikal: "https://i-viaplay-com.akamaized.net/viaplay-prod/46/296/1664191298-ec1b5dc6156f9832974044dde6bbf7ca43d92a9b.jpg?width=400&height=600", // portrett
+    beskrivelse: "Elvis' historie skildres gjennom det kompliserte forholdet til hans manager, oberst Tom Parker. Det var en innfløkt dynamikk mellom dem i over 20 år.",
+    metadata: ["USA", "2022", "12+", "Drama / Historie", "2 t 40 min"],
+    imdb: "https://www.imdb.com/title/tt3704428/?ref_=nv_sr_srsg_0_tt_4_nm_4_in_0_q_Elvis",
+    skuespillere: "Austin Butler, Tom Hanks, Olivia DeJonge, Richard Roxburgh, Helen Thomson",
+    regissor: "Baz Luhrmann",
+    watchUrl: "film-mal.html?id=elvis"
+  },
+
+  "comet-in-moominland": {
+    tittel: "Comet in Moominland",
+    bakgrunn: "https://assets.mubicdn.net/images/film/95372/image-w1280.jpg",
+    logo: "https://static.wixstatic.com/media/fd2fb2_1c6bc07b8c5e40808ccc2a345870b061~mv2.png",
+    poster: "https://static.wixstatic.com/media/fd2fb2_0639d85789b84e0b84e29d09a21632e2~mv2.jpg",
+    posterVertikal: "https://m.media-amazon.com/images/M/MV5BYjVjZGViNmEtNmZmNS00OThjLWIxZWQtZjVhYmNmYWUzZDExXkEyXkFqcGc@._V1_.jpg", // portrett
+    beskrivelse: "Mummi, Sniff og My legger ut for å stoppe en komet som nærmer seg Mummidalen.",
+    metadata: ["Finland", "1992", "Alle", "Familie / Barn", "1 t 10 min"],
+    imdb: "https://www.imdb.com/title/tt0240425/?ref_=nv_sr_srsg_0_tt_8_nm_0_in_0_q_Comet%2520in%2520moo",
+    skuespillere: "Rabbe Smedlund, Jyrki Kovaleff, Elina Salo",
+    regissor: "Hiroshi Saitô",
+    watchUrl: "film-mal.html?id=comet-in-moominland"
+  },
+
+    "kongens-nei": {
+    tittel: "Kongens Nei",
+    bakgrunn: "https://i-viaplay-com.akamaized.net/viaplay-prod/440/136/1481804635-4b2b95fad334a58ac54ec2a211744021e4cb0f6a.jpg?width=1600&height=900",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/f/f8/Kongens_nei.png",
+    poster: "https://i-viaplay-com.akamaized.net/viaplay-prod/440/136/1481804637-d3396e82d915956b7b435991d4b13d7b0113256a.jpg?width=448&height=252",
+    posterVertikal: "https://i-viaplay-com.akamaized.net/viaplay-prod/440/136/1481804635-e2e21566cce4cb5887c08d8aa8358e78759595cf.jpg?width=400&height=600", // portrett
+    beskrivelse: "Kongens nei handler om de tre dramatiske døgnene i april 1940, hvor den norske kongen får et ufattelig ultimatum av den tyske krigsmakten: Overgivelse eller døden.",
+    metadata: ["Norge", "2016", "9+", "Drama / Krig", "2 t 13 min"],
+    imdb: "https://www.imdb.com/title/tt4353996/",
+    skuespillere: "Anders Baasmo Christiansen, Erik Hivju, Jesper Christensen, Rolf Kristian Larsen, Tuva Novotny",
+    regissor: "Erik Poppe",
+    watchUrl: "film-mal.html?id=kongens-nei"
+  },
+
+     "max-manus": {
+    tittel: "Max Manus",
+    bakgrunn: "https://image.tmdb.org/t/p/w1280/ji8AsF2vqsg88LzbOXakGGbRXSC.jpg",
+    logo: "https://occ-0-1165-38.1.nflxso.net/dnm/api/v6/S4oi7EPZbv2UEPaukW54OORa0S8/AAAABVm_bCxSsU6UwHkFG5da1LoOjS65NJzSrYA0Q_IWBV5k3_1_2Vrc5pDAPup2rhZHifNDclGG-3N1uUb-JwrYM20_xlVXLDyl-g.webp?r=04c",
+    poster: "https://sumo.cdn.tv2.no/imageapi/v3/img/65c76cd7498e720ede899535-1738749898075/nfpluss?location=list&width=1200&height=630",
+    posterVertikal: "https://i-viaplay-com.akamaized.net/viaplay-prod/715/824/1635457976-80bb0806bfbb1af04f28b7addedc4b5458878dd3.jpg?width=400&height=600", // portrett
+    beskrivelse: "Filmen bygger på den virkelige historien om Max Manus, det mest fargerike medlem av den norske motstandsbevegelsen under andre verdenskrig.",
+    metadata: ["Norge", "2008", "15+", "Drama / Krig", "1 t 58 min"],
+    imdb: "https://www.imdb.com/title/tt1029235/",
+    skuespillere: "Aksel Hennie, Agnes Kittelsen, Nicolai Cleve Broch",
+    regissor: "Joachim Rønning, Espen Sandberg",
+    watchUrl: "film-mal.html?id=max-manus"
+  },
+       "smurfs": {
+    tittel: "Smurfs",
+    bakgrunn: "https://image.tmdb.org/t/p/original/9whEVuKte4Qi0LI4TzPf7glinJW.jpg",
+    logo: "https://static.wikia.nocookie.net/international-entertainment-project/images/2/21/Smurfs_%282025%29_-_logo_%28English%29.png/revision/latest/scale-to-width-down/1200?cb=20250207014554",
+    poster: "https://hype.my/wp-content/uploads/2025/07/Smurfs-Header.png",
+    posterVertikal: "https://image.tmdb.org/t/p/original/3dwNXK6A5Jmw7h4JRqsq2tQIreQ.jpg", // portrett
+    beskrivelse: "Når Gammelsmurf blir tatt av de onde trollmennene Razamel og Gargamel, leder Smurfette Smurfene på et oppdrag til den virkelige verden for å redde ham.",
+    metadata: ["USA", "2025", "7+", "Animasjon / Familie", "1 t 29 min"],
+    imdb: "https://www.imdb.com/title/tt18069420/",
+    skuespillere: "Aksel Hennie, Agnes Kittelsen, Nicolai Cleve Broch",
+    regissor: "Joachim Rønning, Espen Sandberg",
+    watchUrl: "film-mal.html?id=max-manus"
+  },
+};
