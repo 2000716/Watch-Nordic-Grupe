9b30829c9c8986262a98f73d8786e4acf72ce70d
2000716 <SveinOlavHaukaasArstein@hotmail.com>
2025-09-18 20:11:39 +0200

    Update Filmdata.js

--- Filmdata.js
+++ Filmdata.js
@@ -10,8 +10,6 @@ const filmer = {
       { code: "no", label: "Norsk" },
       { code: "off", label: "Av" }
     ],
-
-    // 🔽 nye felter lagt til:
     bakgrunn: "https://beam-images.warnermediacdn.com/BEAM_LWM_DELIVERABLES/d28771bd-73ed-49a7-9259-bdb33a115cbc/c628867cbdcd9987018dd7e994bde435f4725dac.jpg?host=wbd-images.prod-vod.h264.io&partner=beamcom",
     logo: "https://occ-0-8407-92.1.nflxso.net/dnm/api/v6/S4oi7EPZbv2UEPaukW54OORa0S8/AAAABXsHIUIEsoNXp18ZFMeNZGV2VDqmkxZieG_LfgHTMtvUNwYCxJXmI4SMgxIdomfNtMLREHe7vqwoceGjEqzTsD-aHT8veWO5GKQ.png?r=e74",
     poster: "https://is1-ssl.mzstatic.com/image/thumb/Video124/v4/4b/f6/66/4bf66634-eace-f237-8991-a383189e2b03/pr_source.lsr/392x220.webp",
@@ -34,76 +32,116 @@ const filmer = {
       { code: "no", label: "Norsk" },
       { code: "off", label: "Av" }
     ],
-    
     bakgrunn: "https://d32qys9a6wm9no.cloudfront.net/images/movies/backdrop/ae/3d838f72e7bea39f327766bd55957a15_1280x720.jpg?t=1636109773",
     logo: "https://static.wixstatic.com/media/fd2fb2_dac88f2b81fb43858c47fc29a2fe3952~mv2.png",
     poster: "https://static.wixstatic.com/media/fd2fb2_3b5f5cd114b746e8b05881724a22a0e5~mv2.jpg",
-    posterVertikal: "https://image.tmdb.org/t/p/original/a8xMPQUw8h7E3bJ9Dw596SD8p3v.jpg", // portrett
+    posterVertikal: "https://image.tmdb.org/t/p/original/a8xMPQUw8h7E3bJ9Dw596SD8p3v.jpg",
     beskrivelse: "Den middelaldrende advokaten Peter Banning må overbevises om at han en gang var den legendariske Peter Pan for å kunne redde barna fra Kaptein Krok.",
     metadata: ["USA", "1991", "13+", "Fantasi / Familie", "2 t 16 min"],
     imdb: "https://www.imdb.com/title/tt0102057/?ref_=nv_sr_srsg_1_tt_5_nm_2_in_0_q_HOOK",
     skuespillere: "Dustin Hoffman, Robin Williams, Julia Roberts",
-    regissor: "Steven Spielberg",
+    regissor: "Steven Spielberg"
   },
 
   "kaptein-sabeltann": {
-    tittel: "Kaptein Sabeltann",
+    tittel: "Kaptein Sabeltann og den magiske diamant",
+    watchUrl: "film-mal.html?id=kaptein-sabeltann",
+    videoUrl: "https://video.wixstatic.com/video/fd2fb2_6e01d7be2fda439fb39701d649b7f8b1/1080p/mp4/file.mp4",
+    subtitleUrl: "Kaptein-sabeltann.VTT",
+    tilbakeUrl: "film.html?navn=kaptein-sabeltann",
+    audioLanguages: [{ code: "no", label: "Norsk" }],
+    subtitleLanguages: [
+      { code: "no", label: "Norsk" },
+      { code: "off", label: "Av" }
+    ],
     bakgrunn: "https://iptvimagecache-playplus-prod-vip.han.telia.se/original/no/imgv3/GnP7NDuB0Rrw7IqKaTNuGddHmo9uH98LTuGRs8nsXJvIiNzbUb2LoKtBRiiR2Yg-OfX6odiC2nMHfm6XZuDZz-iivlYEPLXGpx3QZMpE21N2HaSNGxmn_HIyz30.jpeg",
     logo: "https://static.wixstatic.com/media/fd2fb2_38bcd3561623467f8a05f8fb23488188~mv2.png",
     poster: "https://beam-images.warnermediacdn.com/BEAM_LWM_DELIVERABLES/500cfa5c-264b-4366-9264-342b1569605e/32dcbd1e-b23c-4acd-ba97-2e6b6a42a620?host=wbd-images.prod-vod.h264.io&partner=beamcom&w=500",
-    posterVertikal: "https://i-viaplay-com.akamaized.net/viaplay-prod/316/184/1574841515-a8796d95201d56c1f26a231e3cad4ddd89e2bb01.jpg?width=400&height=600", // portrett
+    posterVertikal: "https://i-viaplay-com.akamaized.net/viaplay-prod/316/184/1574841515-a8796d95201d56c1f26a231e3cad4ddd89e2bb01.jpg?width=400&height=600",
     beskrivelse: "Legenden sier at Den Magiske Diamant oppfyller ønsker ved fullmåne. Men det er vesle Marco som stikker av med diamanten!",
     metadata: ["Norge", "2019", "Alle", "Familie / Barn", "1 t 19 min"],
     imdb: "https://www.imdb.com/title/tt8048384/?ref_=nv_sr_srsg_0_tt_8_nm_0_in_0_q_kaptein%2520sabeltann%2520og%2520den%2520",
     skuespillere: "Kyrre Haugen Sydness, Siri Skjeggedal, Leonard Valestrand Eike",
-    regissor: "Marit Moum Aune, Rasmus A. Sivertsen",
-    watchUrl: "film-mal.html?id=kaptein-sabeltann"
+    regissor: "Marit Moum Aune, Rasmus A. Sivertsen"
   },
 
   "burlesque": {
     tittel: "Burlesque",
+    watchUrl: "film-mal.html?id=burlesque",
+    videoUrl: "https://video.wixstatic.com/video/fd2fb2_7462cc0c8b1741fe813b5bf84c073e84/720p/mp4/file.mp4",
+    subtitleUrl: "Burlesque.VTT",
+    tilbakeUrl: "film.html?navn=burlesque",
+    audioLanguages: [{ code: "en", label: "Engelsk" }],
+    subtitleLanguages: [
+      { code: "no", label: "Norsk" },
+      { code: "off", label: "Av" }
+    ],
     bakgrunn: "https://occ-0-8407-2218.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABTbfoDZNprg4kKg_Azptknl68ULWGPAhGrKE9Z-7ZFiyg57lAt1CHnWYz8e8cWjI-oKpRnNMfMBKb7bFRi6mBtAdnqQ3rNanOmDC.jpg?r=210",
     logo: "https://upload.wikimedia.org/wikipedia/fr/d/d0/Burlesque_%28film%2C_2010%29_Logo.png",
     poster: "https://static.wixstatic.com/media/fd2fb2_c85b71dfd8bc4cc6ae7ac39b8513370d~mv2.jpg",
-    posterVertikal: "https://m.media-amazon.com/images/I/81qjANQ8iPL._UF894,1000_QL80_.jpg", // portrett
+    posterVertikal: "https://m.media-amazon.com/images/I/81qjANQ8iPL._UF894,1000_QL80_.jpg",
     beskrivelse: "Etter å ha dratt fra Iowa med stjerner i øynene, kommer Ali til Los Angeles og nattklubben Burlesque med en drøm om å ta klubben med storm med sopranstemmen.",
     metadata: ["USA", "2010", "8+", "Musikal", "1 t 59 min"],
     imdb: "https://www.imdb.com/title/tt1126591/?ref_=nv_sr_srsg_0_tt_8_nm_0_in_0_q_burlesque",
     skuespillere: "Cher, Christina Aguilera, Alan Cumming",
-    regissor: "Steve Antin",
-    watchUrl: "film-mal.html?id=burlesque"
+    regissor: "Steve Antin"
   },
 
   "dog-man": {
-    tittel: "Dog Man",
+    tittel: "Hundemannen",
+    watchUrl: "film-mal.html?id=dog-man",
+    videoUrl: "https://video.wixstatic.com/video/fd2fb2_f18a5454a4584c6e9b4d7f6e1e97413a/1080p/mp4/file.mp4",
+    subtitleUrl: "Dog-man.VTT",
+    tilbakeUrl: "film.html?navn=dog-man",
+    audioLanguages: [{ code: "no", label: "Norsk" }],
+    subtitleLanguages: [
+      { code: "no", label: "Norsk" },
+      { code: "off", label: "Av" }
+    ],
     bakgrunn: "https://andersonvision.com/wp-content/uploads/2025/03/Dog-Man-blu-ray-7-scaled.jpg",
     logo: "https://www.universalpictures.co.uk/tl_files/content/movies/dog_man/title.png",
     poster: "https://m.media-amazon.com/images/S/pv-target-images/44839e4a60bb8205258ba5fb43d1369c8a3cdd27f628692f6d0bd5c6c0c35ca6.jpg",
-    posterVertikal: "https://i-viaplay-com.akamaized.net/viaplay-prod/849/40/1740747124-2e6f862a9638dd9cefc70ddf014a388f6f2ada76.jpg?width=400&height=600", // portrett
+    posterVertikal: "https://i-viaplay-com.akamaized.net/viaplay-prod/849/40/1740747124-2e6f862a9638dd9cefc70ddf014a388f6f2ada76.jpg?width=400&height=600",
     beskrivelse: "Etter at en politimann og politihunden hans smelter sammen for å bli Hundemannen, må han stoppe skurken katten Petey.",
     metadata: ["USA", "2025", "7+", "Barn / Familie", "1 t 29 min"],
     imdb: "https://www.imdb.com/title/tt10954718/?ref_=nv_sr_srsg_1_tt_6_nm_1_in_0_q_dOg%2520man",
     skuespillere: "Pete Davidson, Poppy Liu, Lil Rel Howery",
-    regissor: "Peter Hastings",
-    watchUrl: "film-mal.html?id=dog-man"
+    regissor: "Peter Hastings"
   },
 
   "dyrene-i-hakkabakkeskogen": {
-    tittel: "Dyrene i Hakkebakkeskogen",
+    tittel: "Dyrene i Hakkabakkeskogen",
+    watchUrl: "film-mal.html?id=dyrene-i-hakkabakkeskogen",
+    videoUrl: "https://video.wixstatic.com/video/fd2fb2_e702a89f1d8d4d7b8cecfc4692d04493/720p/mp4/file.mp4",
+    subtitleUrl: "",
+    tilbakeUrl: "film.html?navn=dyrene-i-hakkabakkeskogen",
+    audioLanguages: [{ code: "no", label: "Norsk" }],
+    subtitleLanguages: [],
+    
     bakgrunn: "https://images.squarespace-cdn.com/content/v1/582b59736b8f5b751a0cf841/1483043813486-L79UYGV3MT4F5744E5JV/image-asset.jpeg",
     logo: "https://is1-ssl.mzstatic.com/image/thumb/Features113/v4/15/0e/02/150e0271-d537-03c1-fcce-c94654692013/hqd8703496463884450421.png/760x286.png",
     poster: "https://is1-ssl.mzstatic.com/image/thumb/Video113/v4/b1/96/ac/b196acbf-3010-b760-3193-702c340000f0/1181928424-WW-AMP_SF.lsr/1200x675.jpg",
-    posterVertikal: "https://i-viaplay-com.akamaized.net/viaplay-prod/894/612/1494855562-3d4fd9d992512643629a5255922564376c705923.jpg?width=400&height=600", // portrett
-    beskrivelse: "I en liten skog bor Klatremus og mange andre dyr. Her lever de livet sitt og synger og danser når de har lyst. Men de må passe på for reven.",
-    metadata: ["Norge", "2016", "Alle", "Barn / Familie", "1 t 15 min"],
-    imdb: "https://www.imdb.com/title/tt4730838/?ref_=nv_sr_srsg_0_tt_7_nm_1_in_0_q_dyrene%2520i%2520",
-    skuespillere: "Espen Bråten Kristoffersen, Stig Henrik Hoff, Wenche Myhre",
-    regissor: "Rasmus A. Sivertsen",
-    watchUrl: "film-mal.html?id=dyrene-i-hakkabakkeskogen"
-  },
+    posterVertikal: "https://i-viaplay-com.akamaized.net/viaplay-prod/894/612/1494855562-3d4fd9d992512643629a5255922564376c705923.jpg?width=400&height=600",
+    beskrivelse: "I en liten skog bor Klatremus og mange andre dyr. Her lever de livet sitt og synger og danser når de har lyst.",
+    metadata: ["Norge", "2016", "Alle", "Barn / Familie", "1 t 25 min"],
+    imdb: "https://www.imdb.com/title/tt5804324/?ref_=nv_sr_srsg_0",
+    skuespillere: "Odd-Magnus Williamson, Ingar Helge Gimle, Anette Hoff",
+    regissor: "Rasmus A. Sivertsen"
+  }
+};
 
-  "jul-i-flåklypa": {
+ "jul-i-flåklypa": {
     tittel: "Jul i Flåklypa",
+    watchUrl: "film-mal.html?id=jul-i-flåklypa",
+    videoUrl: "https://video.wixstatic.com/video/fd2fb2_26bfed9bfc254af8b14f41820a066eec/720p/mp4/file.mp4",
+    subtitleUrl: "Jul-i-flåklypa.VTT",
+    tilbakeUrl: "film.html?navn=jul-i-flåklypa",
+    audioLanguages: [{ code: "no", label: "Norsk" }],
+    subtitleLanguages: [
+      { code: "no", label: "Norsk" },
+      { code: "off", label: "Av" }
+    ],
+ 
     bakgrunn: "https://occ-0-8407-2218.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABf_jXS7fg-ggm0v_ygwinuf9ZAf46TiEnc5yHc9if-TcxcEsEh_RIDErLYQpvhnosZF6f3oXPzEdY7xdP0XOSM2xcPDK0s6Ix0HM.jpg?r=9e5",
     logo: "https://occ-0-3668-38.1.nflxso.net/dnm/api/v6/S4oi7EPZbv2UEPaukW54OORa0S8/AAAABW5AdcvFX4G8XnB_b3uQRtZgvyU3QTyH4uSjNdDIANcjK_MlWXdMffH6J7JaKlt3Xj4_YKWusqEWZ9Xo1CkqN2_OYV51oVqvhw.webp?r=97e",
     poster: "https://sumo.cdn.tv2.no/imageapi/v3/img/5dbc0a4f498e1dc3eb8394b3-1700644038168",
@@ -249,72 +287,12 @@ const filmer = {
 
  
   
-  "hook": {
-    tittel: "Hook",
-    watchUrl: "film-mal.html?id=hook",
-    videoUrl: "https://video.wixstatic.com/video/fd2fb2_4900b93cc027487693fc66246b1ae100/720p/mp4/file.mp4",
-    subtitleUrl: "Hook.VTT",
-    tilbakeUrl: "film.html?navn=hook",
-    audioLanguages: [{ code: "en", label: "Engelsk" }],
-    subtitleLanguages: [
-      { code: "no", label: "Norsk" },
-      { code: "off", label: "Av" }
-    ]
-  },
-  "kaptein-sabeltann": {
-    tittel: "Kaptein Sabeltann og den magiske diamant",
-    watchUrl: "film-mal.html?id=kaptein-sabeltann",
-    videoUrl: "https://video.wixstatic.com/video/fd2fb2_6e01d7be2fda439fb39701d649b7f8b1/1080p/mp4/file.mp4",
-    subtitleUrl: "Kaptein-sabeltann.VTT",
-    tilbakeUrl: "film.html?navn=kaptein-sabeltann",
-    audioLanguages: [{ code: "no", label: "Norsk" }],
-    subtitleLanguages: [ { code: "no", label: "Norsk" },
-      { code: "off", label: "Av" }]
-  },
-  "burlesque": {
-    tittel: "Burlesque",
-    watchUrl: "film-mal.html?id=burlesque",
-    videoUrl: "https://video.wixstatic.com/video/fd2fb2_7462cc0c8b1741fe813b5bf84c073e84/720p/mp4/file.mp4",
-    subtitleUrl: "Burlesque.VTT",
-    tilbakeUrl: "film.html?navn=burlesque",
-    audioLanguages: [{ code: "en", label: "Engelsk" }],
-    subtitleLanguages: [
-      { code: "no", label: "Norsk" },
-      { code: "off", label: "Av" }
-    ]
-  },
-  "dog-man": {
-    tittel: "Hundemannen",
-    watchUrl: "film-mal.html?id=dog-man",
-    videoUrl: "https://video.wixstatic.com/video/fd2fb2_f18a5454a4584c6e9b4d7f6e1e97413a/1080p/mp4/file.mp4",
-    subtitleUrl: "Dog-man.VTT",
-    tilbakeUrl: "film.html?navn=dog-man",
-    audioLanguages: [{ code: "no", label: "Norsk" }],
-    subtitleLanguages: [
-      { code: "no", label: "Norsk" },
-      { code: "off", label: "Av" }
-    ]
-  },
-  "dyrene-i-hakkabakkeskogen": {
-    tittel: "Dyrene i Hakkabakkeskogen",
-    watchUrl: "film-mal.html?id=dyrene-i-hakkabakkeskogen",
-    videoUrl: "https://video.wixstatic.com/video/fd2fb2_e702a89f1d8d4d7b8cecfc4692d04493/720p/mp4/file.mp4",
-    subtitleUrl: "",
-    tilbakeUrl: "film.html?navn=dyrene-i-hakkabakkeskogen",
-    audioLanguages: [{ code: "no", label: "Norsk" }],
-    subtitleLanguages: []
-  },
-  "jul-i-flåklypa": {
-    tittel: "Jul i Flåklypa",
-    watchUrl: "film-mal.html?id=jul-i-flåklypa",
-    videoUrl: "https://video.wixstatic.com/video/fd2fb2_26bfed9bfc254af8b14f41820a066eec/720p/mp4/file.mp4",
-    subtitleUrl: "Jul-i-flåklypa.VTT",
-    tilbakeUrl: "film.html?navn=jul-i-flåklypa",
-    audioLanguages: [{ code: "no", label: "Norsk" }],
-    subtitleLanguages: [
-      { code: "no", label: "Norsk" },
-      { code: "off", label: "Av" }
-    ]
+ 
+ 
+
+  
+
+
   },
   "bamse-og-dunderklokken": {
     tittel: "Bamse og dunderklokken",
@@ -422,6 +400,7 @@ const filmer = {
   },
 
 };
+
 
 
 
