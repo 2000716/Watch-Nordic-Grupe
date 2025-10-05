1e6b2dbb336ae016cb8662b2485146fbccca08da
2000716 <SveinOlavHaukaasArstein@hotmail.com>
2025-10-05 14:48:55 +0200

    Update Seriedata.js

--- Seriedata.js
+++ Seriedata.js
@@ -1,2 +1,44 @@
+const serier = {
+  "stranger-things": {
+    tittel: "Stranger Things",
+    bakgrunn: "https://occ-0-2774-2773.1.nflxso.net/art/1e1cc/1e1cc36b78849b8c62e5b048dd77d02d7d694a69.jpg",
+    banner: "https://image.tmdb.org/t/p/original/x2LSRK2Cm7MZhjluni1msVJ3wDF.jpg", // hero-banner
+    logo: "https://occ-0-55-56.1.nflxso.net/art/8b26a/8b26a3dfc7b891f55ee4b04ff10d9bcd1b3a8f3a.png",
+    poster: "https://m.media-amazon.com/images/I/71xZs1H9cdL._AC_SL1200_.jpg",
+    posterVertikal: "https://image.tmdb.org/t/p/w780/56v2KjBlU4XaOv9rVYEQypROD7P.jpg",
+    beskrivelse: "Når en ung gutt forsvinner i en liten by, avdekker vennene hans en mystisk jente med overnaturlige krefter og en hemmelig regjeringskonspirasjon.",
+    metadata: ["USA", "2016–", "16+", "Mystikk / Sci-fi / Drama", "4 sesonger"],
+    imdb: "https://www.imdb.com/title/tt4574334/",
+    skuespillere: "Millie Bobby Brown, Finn Wolfhard, David Harbour, Winona Ryder",
+    skapere: "The Duffer Brothers",
+    antallSesonger: 4,
+    antallEpisoder: 34,
+    watchUrl: "serie-mal.html?id=stranger-things",
+    status: "Pågående",
+    expireDate: "2026-12-31T23:59:59",
+  },
+
+  "the-office": {
+    tittel: "The Office (US)",
+    bakgrunn: "https://m.media-amazon.com/images/S/pv-target-images/bcfbfe85fd5c8a4441e8d27b1a6e961a7e6e034b3b909e520d4c5734cb1ee64e._UR1920,1080_SX1500_FMjpg_.jpg",
+    banner: "https://image.tmdb.org/t/p/original/qWnJzyZhyy74gjpSjIXWmuk0ifX.jpg",
+    logo: "https://occ-0-92-93.1.nflxso.net/art/622b5/622b58a22601ffb2ec64f88c8c92d685bc29124b.png",
+    poster: "https://m.media-amazon.com/images/I/81EYlj8ePjL._AC_SY679_.jpg",
+    posterVertikal: "https://image.tmdb.org/t/p/w780/hH4YaZuH89Hlyz0DEkf362Mj8Oa.jpg",
+    beskrivelse: "En tøysete sjef, et håpløst kontorliv og en rekke latterlige situasjoner i Dunder Mifflin Paper Company. En komiserie filmet som en dokumentar.",
+    metadata: ["USA", "2005–2013", "12+", "Komedie", "9 sesonger"],
+    imdb: "https://www.imdb.com/title/tt0386676/",
+    skuespillere: "Steve Carell, Rainn Wilson, John Krasinski, Jenna Fischer",
+    skapere: "Greg Daniels",
+    antallSesonger: 9,
+    antallEpisoder: 201,
+    watchUrl: "serie-mal.html?id=the-office",
+    status: "Fullført",
+    expireDate: "2027-06-15T23:59:59",
+  },
+};
+
+export default serier;
+
 
 
