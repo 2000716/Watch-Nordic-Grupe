0d7cb3de21cab7a370ac0d244e65d89e085d924d
2000716 <SveinOlavHaukaasArstein@hotmail.com>
2025-10-09 15:10:36 +0200

    Update Seriedata.js

--- Seriedata.js
+++ Seriedata.js
@@ -3,20 +3,47 @@ const serier = {
   "i-mummidalen": {
     tittel: "I Mummidalen",
     bakgrunn: "https://gfx.nrk.no/dX0XYs1HRsBtIl6h4ZWoawCp6_b4VgKnn2dGKDMiObpA",
-    banner: "https://image.tmdb.org/t/p/original/x2LSRK2Cm7MZhjluni1msVJ3wDF.jpg", // hero-banner
+    banner: "https://image.tmdb.org/t/p/original/x2LSRK2Cm7MZhjluni1msVJ3wDF.jpg",
     logo: "https://occ-0-55-56.1.nflxso.net/art/8b26a/8b26a3dfc7b891f55ee4b04ff10d9bcd1b3a8f3a.png",
     poster: "https://image.tmdb.org/t/p/w1280/9yjEMASvxhJGSUq7peDpWgBO5JW.jpg",
     posterVertikal: "https://image.tmdb.org/t/p/w780/56v2KjBlU4XaOv9rVYEQypROD7P.jpg",
-    beskrivelse:"De sjarmerende og animerte eventyrene til en skapning som heter Mummi og hans venner og familie.",
-    metadata: ["Finnland", "1990", "Alle", "Familie", "4 sesonger"],
+    beskrivelse:
+      "De sjarmerende og animerte eventyrene til en skapning som heter Mummi og hans venner og familie.",
+    metadata: ["Finland", "1990", "Alle", "Familie", "4 sesonger"],
     imdb: "https://www.imdb.com/title/tt0247117/",
-    skuespillere: "Millie Bobby Brown, Finn Wolfhard, David Harbour, Winona Ryder",
-    skapere: "The Duffer Brothers",
+    skuespillere: "Mummitrollet, Snusmumrikken, Lille My, Snorkfrøken",
+    skapere: "Tove Jansson",
     antallSesonger: 4,
     antallEpisoder: 34,
-    watchUrl: "serie-mal.html?id=stranger-things",
+    watchUrl: "spillerepisode.html?serie=i-mummidalen&s=1&e=1",
     status: "Pågående",
     expireDate: "2026-12-31T23:59:59",
+
+    // 🔹 Legg til episoder her
+    episoder: {
+      "Sesong 1": [
+        {
+          tittel: "Episode 1: Vår i Mummidalen",
+          varighet: "22 min",
+          thumbnail: "https://m.media-amazon.com/images/I/81EYlj8ePjL._AC_SY679_.jpg",
+          watchUrl: "spillerepisode.html?serie=i-mummidalen&s=1&e=1"
+        },
+        {
+          tittel: "Episode 2: Snusmumrikken vender tilbake",
+          varighet: "21 min",
+          thumbnail: "https://m.media-amazon.com/images/I/81EYlj8ePjL._AC_SY679_.jpg",
+          watchUrl: "spillerepisode.html?serie=i-mummidalen&s=1&e=2"
+        }
+      ],
+      "Sesong 2": [
+        {
+          tittel: "Episode 1: Den store flommen",
+          varighet: "23 min",
+          thumbnail: "https://m.media-amazon.com/images/I/81EYlj8ePjL._AC_SY679_.jpg",
+          watchUrl: "spillerepisode.html?serie=i-mummidalen&s=2&e=1"
+        }
+      ]
+    }
   },
 
   "the-office": {
@@ -35,16 +62,39 @@ const serier = {
     skapere: "Greg Daniels",
     antallSesonger: 9,
     antallEpisoder: 201,
-    watchUrl: "serie-mal.html?id=the-office",
+    watchUrl: "spillerepisode.html?serie=the-office&s=1&e=1",
     status: "Fullført",
     expireDate: "2027-06-15T23:59:59",
-  },
+
+    episoder: {
+      "Sesong 1": [
+        {
+          tittel: "Episode 1: Pilot",
+          varighet: "23 min",
+          thumbnail: "https://image.tmdb.org/t/p/w300/9HH4YaZuH89Hlyz0DEkf362Mj8Oa.jpg",
+          watchUrl: "spillerepisode.html?serie=the-office&s=1&e=1"
+        },
+        {
+          tittel: "Episode 2: Diversity Day",
+          varighet: "22 min",
+          thumbnail: "https://image.tmdb.org/t/p/w300/hH4YaZuH89Hlyz0DEkf362Mj8Oa.jpg",
+          watchUrl: "spillerepisode.html?serie=the-office&s=1&e=2"
+        }
+      ],
+      "Sesong 2": [
+        {
+          tittel: "Episode 1: The Dundies",
+          varighet: "21 min",
+          thumbnail: "https://image.tmdb.org/t/p/w300/aBcDeFgH123.jpg",
+          watchUrl: "spillerepisode.html?serie=the-office&s=2&e=1"
+        }
+      ]
+    }
+  }
 };
 
 // Gjør tilgjengelig globalt
 window.serier = serier;
-
-
 
 
 
