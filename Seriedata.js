7c5052c0e900a76f7136f2cf7015278537257892
2000716 <SveinOlavHaukaasArstein@hotmail.com>
2025-10-11 22:59:45 +0200

    Update Seriedata.js

--- Seriedata.js
+++ Seriedata.js
@@ -46,55 +46,10 @@ const serier = {
     }
   },
 
-  "the-office": {
-    tittel: "The Office (US)",
-    bakgrunn:
-      "https://m.media-amazon.com/images/S/pv-target-images/bcfbfe85fd5c8a4441e8d27b1a6e961a7e6e034b3b909e520d4c5734cb1ee64e._UR1920,1080_SX1500_FMjpg_.jpg",
-    banner: "https://image.tmdb.org/t/p/original/qWnJzyZhyy74gjpSjIXWmuk0ifX.jpg",
-    logo: "https://occ-0-92-93.1.nflxso.net/art/622b5/622b58a22601ffb2ec64f88c8c92d685bc29124b.png",
-    poster: "https://m.media-amazon.com/images/I/81EYlj8ePjL._AC_SY679_.jpg",
-    posterVertikal: "https://image.tmdb.org/t/p/w780/hH4YaZuH89Hlyz0DEkf362Mj8Oa.jpg",
-    beskrivelse:
-      "En tøysete sjef, et håpløst kontorliv og en rekke latterlige situasjoner i Dunder Mifflin Paper Company. En komiserie filmet som en dokumentar.",
-    metadata: ["USA", "2005–2013", "12+", "Komedie", "9 sesonger"],
-    imdb: "https://www.imdb.com/title/tt0386676/",
-    skuespillere: "Steve Carell, Rainn Wilson, John Krasinski, Jenna Fischer",
-    skapere: "Greg Daniels",
-    antallSesonger: 9,
-    antallEpisoder: 201,
-    watchUrl: "spillerepisode.html?serie=the-office&s=1&e=1",
-    status: "Fullført",
-    expireDate: "2027-06-15T23:59:59",
-
-    episoder: {
-      "Sesong 1": [
-        {
-          tittel: "Episode 1: Pilot",
-          varighet: "23 min",
-          thumbnail: "https://image.tmdb.org/t/p/w300/9HH4YaZuH89Hlyz0DEkf362Mj8Oa.jpg",
-          watchUrl: "spillerepisode.html?serie=the-office&s=1&e=1"
-        },
-        {
-          tittel: "Episode 2: Diversity Day",
-          varighet: "22 min",
-          thumbnail: "https://image.tmdb.org/t/p/w300/hH4YaZuH89Hlyz0DEkf362Mj8Oa.jpg",
-          watchUrl: "spillerepisode.html?serie=the-office&s=1&e=2"
-        }
-      ],
-      "Sesong 2": [
-        {
-          tittel: "Episode 1: The Dundies",
-          varighet: "21 min",
-          thumbnail: "https://image.tmdb.org/t/p/w300/aBcDeFgH123.jpg",
-          watchUrl: "spillerepisode.html?serie=the-office&s=2&e=1"
-        }
-      ]
-    }
-  }
-};
-
+ 
 // Gjør tilgjengelig globalt
 window.serier = serier;
+
 
 
 
