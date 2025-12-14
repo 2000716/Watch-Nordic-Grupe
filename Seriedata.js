2529a064c7e2c06eeb271823daf3498e27850200
2000716 <SveinOlavHaukaasArstein@hotmail.com>
2025-12-14 22:56:39 +0100

    Update Seriedata.js

--- Seriedata.js
+++ Seriedata.js
@@ -1,6 +1,6 @@
 const serier = {
-  "corpse-bride-serien": {
-    tittel: "Corpse Bride – Serien",
+  "i-mummidalen": {
+    tittel: "I Mummidalen",
     bakgrunn: "https://image.tmdb.org/t/p/original/231meoDrNjyUqF0sD0xItVw1h7g.jpg",
     logo: "https://image.tmdb.org/t/p/original/cD5TMSmqtRMsuGFCD7QtSWH2bot.png",
     poster: "https://image.tmdb.org/t/p/w1280/9yjEMASvxhJGSUq7peDpWgBO5JW.jpg",
@@ -64,6 +64,7 @@ const serier = {
     }
   }
 };
+
 
 
 
