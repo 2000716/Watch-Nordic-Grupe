4ec554a12ae3558e79f78c9e710a0beab2542a81
2000716 <SveinOlavHaukaasArstein@hotmail.com>
2026-01-29 11:55:33 +0100

    Update Filmdata.js

--- Filmdata.js
+++ Filmdata.js
@@ -1,12 +1,12 @@
 const filmer = {
   "corpse-bride": {
     tittel: "Corpse Bride",
-    bakgrunn: "https://i-viaplay-com.akamaized.net/viaplay-prod/215/1000/1460156720-16bb7db7ba76e552a8dccce6a762ae68f7b22828.jpg?width=1600&height=900",
-    banner: "https://image.tmdb.org/t/p/original/eL8fgEK8mgDcJZRHzuxbLwevCcr.jpg", 
-    logo: "https://occ-0-8407-92.1.nflxso.net/dnm/api/v6/S4oi7EPZbv2UEPaukW54OORa0S8/AAAABXsHIUIEsoNXp18ZFMeNZGV2VDqmkxZieG_LfgHTMtvUNwYCxJXmI4SMgxIdomfNtMLREHe7vqwoceGjEqzTsD-aHT8veWO5GKQ.png?r=e74",
+    bakgrunn: "https://image.tmdb.org/t/p/original/cvWzCDo3pQyoasvqzRIP7oHK3qn.jpg",
+    banner: "https://image.tmdb.org/t/p/original/cvWzCDo3pQyoasvqzRIP7oHK3qn.jpg", 
+    logo: "https://static.wixstatic.com/media/fd2fb2_33ed1fd17c8547729b92df6e099d2cc4~mv2.png",
     trailer: "https://video.wixstatic.com/video/fd2fb2_516f105ae8b94969abd830baf696f6d4/360p/mp4/file.mp4", // 🎥 NY 
-    poster: "https://is1-ssl.mzstatic.com/image/thumb/Video124/v4/4b/f6/66/4bf66634-eace-f237-8991-a383189e2b03/pr_source.lsr/392x220.webp",
-    posterVertikal: "https://image.tmdb.org/t/p/w780/isb2Qow76GpqYmsSyfdMfsYAjts.jpg", // portrett
+    poster: "https://image.tmdb.org/t/p/original/pMeqhS16RXQhNw2hpBkDvBR7r7Z.jpg",
+    posterVertikal: "https://image.tmdb.org/t/p/original/cIIsmZ4az8AaJJoDgtIOS797RCA.jpg", // portrett
     beskrivelse: "På tampen av bryllupet sitt gifter den nervøse brudgommen Victor seg ved et uhell med likbruden Emily og stiger ned med henne til underverdenen.",
     metadata: ["USA", "2005", "9+", "Grøsser", "1 t 13 min"],
     imdb: "https://www.imdb.com/title/tt0121164/?ref_=nv_sr_srsg_1_tt_5_nm_3_in_0_q_corpse%2520",
@@ -25,6 +25,7 @@ const filmer = {
    publishDate: "2025-10-02T00:00:00", // ISO datoformat
     expireDate: "2027-11-15T23:59:59",
   },
+
 
 
 
