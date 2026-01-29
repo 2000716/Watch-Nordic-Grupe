dcc324d9c8bd54ec68f05d7ee8e239bfefe6f136
2000716 <SveinOlavHaukaasArstein@hotmail.com>
2026-01-29 12:03:42 +0100

    Update Filmdata.js

--- Filmdata.js
+++ Filmdata.js
@@ -1,21 +1,21 @@
 const filmer = {
-  "corpse-bride": {
-    tittel: "Corpse Bride",
+  "the-kid": {
+    tittel: "The Kid",
     bakgrunn: "https://image.tmdb.org/t/p/original/cvWzCDo3pQyoasvqzRIP7oHK3qn.jpg",
     banner: "https://image.tmdb.org/t/p/original/cvWzCDo3pQyoasvqzRIP7oHK3qn.jpg", 
-    logo: "https://static.wixstatic.com/media/fd2fb2_33ed1fd17c8547729b92df6e099d2cc4~mv2.png",
-    trailer: "https://video.wixstatic.com/video/fd2fb2_516f105ae8b94969abd830baf696f6d4/360p/mp4/file.mp4", // 🎥 NY 
+    logo: "https://static.wixstatic.com/media/fd2fb2_f0631af4ae814e0087cc49b08ee273e9~mv2.png",
+    trailer: "", // 🎥 NY 
     poster: "https://image.tmdb.org/t/p/original/pMeqhS16RXQhNw2hpBkDvBR7r7Z.jpg",
     posterVertikal: "https://image.tmdb.org/t/p/original/cIIsmZ4az8AaJJoDgtIOS797RCA.jpg", // portrett
-    beskrivelse: "På tampen av bryllupet sitt gifter den nervøse brudgommen Victor seg ved et uhell med likbruden Emily og stiger ned med henne til underverdenen.",
-    metadata: ["USA", "2005", "9+", "Grøsser", "1 t 13 min"],
-    imdb: "https://www.imdb.com/title/tt0121164/?ref_=nv_sr_srsg_1_tt_5_nm_3_in_0_q_corpse%2520",
-    skuespillere: "Johnny Depp, Helena Bonham Carter, Emily Watson",
-    regissor: "Tim Burton",
-    watchUrl: "film-mal.html?id=corpse-bride",
+    beskrivelse: "En mor efterlader sit nyfødte barn i en limousine. På en seddel bønfalder hun ejeren om at tage sig af drengen. Men bilen bliver stjålet at tyve, der smider barnet i skraldespanden.",
+    metadata: ["USA", "1921", "Alle", "Komedie", "1 t 8 min"],
+    imdb: "https://www.imdb.com/title/tt0012349/?ref_=ttfc_ov_bk",
+    skuespillere: "Charles Chaplin, Edna Purviance, Jackie Coogan",
+    regissor: "Charles Chaplin",
+    watchUrl: "film-mal.html?id=the-kid",
     videoUrl:"",
    subtitleUrl: "",
-   tilbakeUrl: "film.html?navn=corpse-bride",
+   tilbakeUrl: "film.html?navn=the-kid",
   audioLanguages: [
     { code: "no", label: "Engelsk" } ],
   subtitleLanguages: [
@@ -26,6 +26,7 @@ const filmer = {
     expireDate: "2027-11-15T23:59:59",
   },
   };
+
 
 
 
