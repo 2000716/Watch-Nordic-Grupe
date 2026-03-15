70ae6d8fc4cc5cf21668d3010a9bf46d1f1a6b3b
2000716 <SveinOlavHaukaasArstein@hotmail.com>
2026-03-15 14:03:15 +0100

    Update Seriedata.js

--- Seriedata.js
+++ Seriedata.js
@@ -1,61 +1,77 @@
 const serier = {
+
   "i-mummidalen": {
+
     tittel: "I Mummidalen",
+
     bakgrunn: "https://image.tmdb.org/t/p/original/231meoDrNjyUqF0sD0xItVw1h7g.jpg",
     logo: "https://image.tmdb.org/t/p/original/cD5TMSmqtRMsuGFCD7QtSWH2bot.png",
+
     poster: "https://image.tmdb.org/t/p/w1280/9yjEMASvxhJGSUq7peDpWgBO5JW.jpg",
     posterVertikal: "https://image.tmdb.org/t/p/original/pU0MiRPaoNeiXZRnJjJ0UOmZiUA.jpg",
+
     beskrivelse:
       "De sjarmerende og animerte eventyrene til en skapning som heter Mummi og hans venner og familie.",
-    metadata: ["Finnland", "1990", "Alle", "Fantasy", "Animasjon"],
+
+    metadata: ["Finland", "1990", "Alle", "Fantasy", "Animasjon"],
+
     imdb: "https://www.imdb.com/title/tt0247117/",
+
     skuespillere: "Johnny Depp, Helena Bonham Carter",
-    regissor: "Tim Burton",
+
+    skapere: "Tim Burton",
 
     expireDate: "2026-11-15T23:59:59",
 
-    audioLanguages: [
-  
-    ],
-    subtitleLanguages: [
+    /* SPRÅK */
+    audioLanguages: [],
+    subtitleLanguages: [],
 
-    ],
-
+    /* SESONGER */
     sesonger: {
+
       1: {
+
         sesongNummer: 1,
         beskrivelse: "Sesong 1 – Begynnelsen",
+
         episoder: {
+
           1: {
             episodeNummer: 1,
             tittel: "Vår i Mummidalen",
+            beskrivelse: "Våren kommer til Mummidalen og nye eventyr begynner.",
             varighet: "24 min",
+
             thumbnail:
               "https://image.tmdb.org/t/p/original/hOpVGMZZ7MYaYjNN2eJzVKFbLVt.jpg",
-            videoUrl:
-              "https://archive.org/details/folder_202411/I+Mummidalen/Season+1/I+Mummidalen+-+1x01+-+V%C3%A5r+i+Mummidalen.mp4",
 
+            videoUrl:
+              "https://archive.org/download/folder_202411/I%20Mummidalen/Season%201/I%20Mummidalen%20-%201x01%20-%20V%C3%A5r%20i%20Mummidalen.mp4"
           },
 
           2: {
             episodeNummer: 2,
-            tittel: "Trollmannsens hatt",
+            tittel: "Trollmannens hatt",
+            beskrivelse: "Mummitrollet finner en mystisk hatt som skaper problemer.",
             varighet: "24 min",
+
             thumbnail:
               "https://i.ytimg.com/vi/S2cmnlwM1n0/maxresdefault.jpg",
+
             videoUrl:
-              "https://archive.org/details/folder_202411/I+Mummidalen/Season+1/I+Mummidalen+-+1x02+-+Trollmannens+hatt.mp4",
+              "https://archive.org/download/folder_202411/I%20Mummidalen/Season%201/I%20Mummidalen%20-%201x02%20-%20Trollmannens%20hatt.mp4"
           }
+
         }
+
       }
+
     }
+
   }
+
 };
-
-
-
-
-
 
 
 
