00b24c2ca4471b902f2f964107aee93317f0c36b
2000716 <SveinOlavHaukaasArstein@hotmail.com>
2026-04-04 00:44:29 +0200

    Refactor series data structure for clarity

--- Seriedata.js
+++ Seriedata.js
@@ -1,7 +1,7 @@
 const serier = {
   "i-mummidalen": {
     tittel: "I Mummidalen",
-    tilbakeUrl: "serier.html", // <=== legg til her
+    tilbakeUrl: "serier.html",
 
     bakgrunn: "https://image.tmdb.org/t/p/original/231meoDrNjyUqF0sD0xItVw1h7g.jpg",
     logo: "https://image.tmdb.org/t/p/original/cD5TMSmqtRMsuGFCD7QtSWH2bot.png",
@@ -25,7 +25,7 @@ const serier = {
           1: {
             episodeNummer: 1,
             tittel: "Vår i Mummidalen",
-            beskrivelse: " Endelig er det vår og Mummitrollet begir seg ut på oppdagelsesferd sammen med Snusmumrikken og Sniff. ",
+            beskrivelse: "Endelig er det vår og Mummitrollet begir seg ut på oppdagelsesferd sammen med Snusmumrikken og Sniff.",
             varighet: "23 min",
             thumbnail: "https://media.themoviedb.org/t/p/w454_and_h254_face/hOpVGMZZ7MYaYjNN2eJzVKFbLVt.jpg",
             tilbakeUrl: "serie.html?navn=i-mummidalen",
@@ -38,7 +38,7 @@ const serier = {
             varighet: "23 min",
             thumbnail: "https://media.themoviedb.org/t/p/w454_and_h254_face/mlKjfZ60zdcNOCd8QH3YHYHIDMd.jpg",
             videoUrl: "https://archive.org/download/folder_202411/I%20Mummidalen/Season%201/I%20Mummidalen%20-%201x02%20-%20Trollmannens%20hatt.mp4"
-           },
+          },
           3: {
             episodeNummer: 3,
             tittel: "Båten på stranden",
@@ -46,7 +46,7 @@ const serier = {
             varighet: "23 min",
             thumbnail: "https://media.themoviedb.org/t/p/w454_and_h254_face/yx65m4QrcIsaPMoWOlOazliUeFD.jpg",
             videoUrl: "https://archive.org/download/folder_202411/I%20Mummidalen/Season%201/I%20Mummidalen%20-%201x03%20-%20B%C3%A5ten%20p%C3%A5%20stranden.mp4"
-             },
+          },
           4: {
             episodeNummer: 4,
             tittel: "Hatifnattøya",
@@ -54,7 +54,7 @@ const serier = {
             varighet: "23 min",
             thumbnail: "https://media.themoviedb.org/t/p/w454_and_h254_face/eAG8eadBIprNbONQwW15zPtktzr.jpg",
             videoUrl: "https://archive.org/download/folder_202411/I%20Mummidalen/Season%201/I%20Mummidalen%20-%201x04%20-%20Hatifnatt%C3%B8ya.mp4"
-             },
+          },
           5: {
             episodeNummer: 5,
             tittel: "Tredronningen",
@@ -62,15 +62,12 @@ const serier = {
             varighet: "23 min",
             thumbnail: "https://media.themoviedb.org/t/p/w454_and_h254_face/fUS845xMJYwAj0X5DynWvdJyQ88.jpg",
             videoUrl: "https://archive.org/download/folder_202411/I%20Mummidalen/Season%201/I%20Mummidalen%20-%201x05%20-%20Tredronningen.mp4"
-             },
+          }
+        }
       }
     }
   }
 };
-
-
-
-
 
 
 
