9291c20c011bc83de144779dc6c4b9270380b990
2000716 <SveinOlavHaukaasArstein@hotmail.com>
2026-04-04 00:41:27 +0200

    Update Seriedata.js

--- Seriedata.js
+++ Seriedata.js
@@ -25,21 +25,44 @@ const serier = {
           1: {
             episodeNummer: 1,
             tittel: "Vår i Mummidalen",
-            beskrivelse: "Våren kommer til Mummidalen og nye eventyr begynner.",
-            varighet: "24 min",
-            thumbnail: "https://image.tmdb.org/t/p/original/hOpVGMZZ7MYaYjNN2eJzVKFbLVt.jpg",
+            beskrivelse: " Endelig er det vår og Mummitrollet begir seg ut på oppdagelsesferd sammen med Snusmumrikken og Sniff. ",
+            varighet: "23 min",
+            thumbnail: "https://media.themoviedb.org/t/p/w454_and_h254_face/hOpVGMZZ7MYaYjNN2eJzVKFbLVt.jpg",
             tilbakeUrl: "serie.html?navn=i-mummidalen",
             videoUrl: "https://archive.org/download/folder_202411/I%20Mummidalen/Season%201/I%20Mummidalen%20-%201x01%20-%20V%C3%A5r%20i%20Mummidalen.mp4"
           },
           2: {
             episodeNummer: 2,
             tittel: "Trollmannens hatt",
-            beskrivelse: "Mummitrollet finner en mystisk hatt som skaper problemer.",
-            varighet: "24 min",
-            thumbnail: "https://i.ytimg.com/vi/S2cmnlwM1n0/maxresdefault.jpg",
+            beskrivelse: "Den magiske hatten som de har prøvd å bli kvitt tidligere, dukker opp igjen.",
+            varighet: "23 min",
+            thumbnail: "https://media.themoviedb.org/t/p/w454_and_h254_face/mlKjfZ60zdcNOCd8QH3YHYHIDMd.jpg",
             videoUrl: "https://archive.org/download/folder_202411/I%20Mummidalen/Season%201/I%20Mummidalen%20-%201x02%20-%20Trollmannens%20hatt.mp4"
-          }
-        }
+           },
+          3: {
+            episodeNummer: 3,
+            tittel: "Båten på stranden",
+            beskrivelse: "Mummitrollet og vennene går på fjelltur. De møter Hemulen, som er lei seg. Han har nemlig samlet sitt siste frimerke, slik at samlingen hans nå er komplett, og hva skal han nå samle på?",
+            varighet: "23 min",
+            thumbnail: "https://media.themoviedb.org/t/p/w454_and_h254_face/yx65m4QrcIsaPMoWOlOazliUeFD.jpg",
+            videoUrl: "https://archive.org/download/folder_202411/I%20Mummidalen/Season%201/I%20Mummidalen%20-%201x03%20-%20B%C3%A5ten%20p%C3%A5%20stranden.mp4"
+             },
+          4: {
+            episodeNummer: 4,
+            tittel: "Hatifnattøya",
+            beskrivelse: "Mummifamilien og vennene deres drar ut på eventyr i den nyreparerte båten, og de ankommer snart en øde øy.",
+            varighet: "23 min",
+            thumbnail: "https://media.themoviedb.org/t/p/w454_and_h254_face/eAG8eadBIprNbONQwW15zPtktzr.jpg",
+            videoUrl: "https://archive.org/download/folder_202411/I%20Mummidalen/Season%201/I%20Mummidalen%20-%201x04%20-%20Hatifnatt%C3%B8ya.mp4"
+             },
+          5: {
+            episodeNummer: 5,
+            tittel: "Tredronningen",
+            beskrivelse: "Våre venner har glemt å fortøye båten skikkelig i stormen, og de blir angrepet av mange Hatifnatter. Stormen bringer med seg mange, rare ting, og det beste funnet gjør Snusmumriken.",
+            varighet: "23 min",
+            thumbnail: "https://media.themoviedb.org/t/p/w454_and_h254_face/fUS845xMJYwAj0X5DynWvdJyQ88.jpg",
+            videoUrl: "https://archive.org/download/folder_202411/I%20Mummidalen/Season%201/I%20Mummidalen%20-%201x05%20-%20Tredronningen.mp4"
+             },
       }
     }
   }
