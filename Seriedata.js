801647d3b4c958d9797700b276745623bee57f28
2000716 <SveinOlavHaukaasArstein@hotmail.com>
2026-04-04 23:37:55 +0200

    Add intro timing to episodes in Seriedata.js
    Added introStart and introEnd properties to episodes.

--- Seriedata.js
+++ Seriedata.js
@@ -27,7 +27,9 @@ const serier = {
             beskrivelse: "Endelig er det vår og Mummitrollet begir seg ut på oppdagelsesferd sammen med Snusmumrikken og Sniff.",
             varighet: "23 min",
             thumbnail: "https://media.themoviedb.org/t/p/w454_and_h254_face/hOpVGMZZ7MYaYjNN2eJzVKFbLVt.jpg",
-            videoUrl: "https://archive.org/download/folder_202411/I%20Mummidalen/Season%201/I%20Mummidalen%20-%201x01%20-%20V%C3%A5r%20i%20Mummidalen.mp4"
+            videoUrl: "https://archive.org/download/folder_202411/I%20Mummidalen/Season%201/I%20Mummidalen%20-%201x01%20-%20V%C3%A5r%20i%20Mummidalen.mp4",
+            introStart: 2,
+            introEnd: 89
           },
           2: {
             episodeNummer: 2,
@@ -35,7 +37,9 @@ const serier = {
             beskrivelse: "Den magiske hatten som de har prøvd å bli kvitt tidligere, dukker opp igjen.",
             varighet: "23 min",
             thumbnail: "https://media.themoviedb.org/t/p/w454_and_h254_face/mlKjfZ60zdcNOCd8QH3YHYHIDMd.jpg",
-            videoUrl: "https://archive.org/download/folder_202411/I%20Mummidalen/Season%201/I%20Mummidalen%20-%201x02%20-%20Trollmannens%20hatt.mp4"
+            videoUrl: "https://archive.org/download/folder_202411/I%20Mummidalen/Season%201/I%20Mummidalen%20-%201x02%20-%20Trollmannens%20hatt.mp4",
+            introStart: 2,
+            introEnd: 89
           },
           3: {
             episodeNummer: 3,
@@ -43,7 +47,9 @@ const serier = {
             beskrivelse: "Mummitrollet og vennene går på fjelltur. De møter Hemulen, som er lei seg. Han har nemlig samlet sitt siste frimerke, slik at samlingen hans nå er komplett, og hva skal han nå samle på?",
             varighet: "23 min",
             thumbnail: "https://media.themoviedb.org/t/p/w454_and_h254_face/yx65m4QrcIsaPMoWOlOazliUeFD.jpg",
-            videoUrl: "https://archive.org/download/folder_202411/I%20Mummidalen/Season%201/I%20Mummidalen%20-%201x03%20-%20B%C3%A5ten%20p%C3%A5%20stranden.mp4"
+            videoUrl: "https://archive.org/download/folder_202411/I%20Mummidalen/Season%201/I%20Mummidalen%20-%201x03%20-%20B%C3%A5ten%20p%C3%A5%20stranden.mp4",
+            introStart: 2,
+            introEnd: 89
           },
           4: {
             episodeNummer: 4,
@@ -51,7 +57,9 @@ const serier = {
             beskrivelse: "Mummifamilien og vennene deres drar ut på eventyr i den nyreparerte båten, og de ankommer snart en øde øy.",
             varighet: "23 min",
             thumbnail: "https://media.themoviedb.org/t/p/w454_and_h254_face/eAG8eadBIprNbONQwW15zPtktzr.jpg",
-            videoUrl: "https://archive.org/download/folder_202411/I%20Mummidalen/Season%201/I%20Mummidalen%20-%201x04%20-%20Hatifnatt%C3%B8ya.mp4"
+            videoUrl: "https://archive.org/download/folder_202411/I%20Mummidalen/Season%201/I%20Mummidalen%20-%201x04%20-%20Hatifnatt%C3%B8ya.mp4",
+            introStart: 2,
+            introEnd: 89
           },
           5: {
             episodeNummer: 5,
@@ -59,7 +67,9 @@ const serier = {
             beskrivelse: "Våre venner har glemt å fortøye båten skikkelig i stormen, og de blir angrepet av mange Hatifnatter. Stormen bringer med seg mange, rare ting, og det beste funnet gjør Snusmumriken.",
             varighet: "23 min",
             thumbnail: "https://media.themoviedb.org/t/p/w454_and_h254_face/fUS845xMJYwAj0X5DynWvdJyQ88.jpg",
-            videoUrl: "https://archive.org/download/folder_202411/I%20Mummidalen/Season%201/I%20Mummidalen%20-%201x05%20-%20Tredronningen.mp4"
+            videoUrl: "https://archive.org/download/folder_202411/I%20Mummidalen/Season%201/I%20Mummidalen%20-%201x05%20-%20Tredronningen.mp4",
+            introStart: 2,
+            introEnd: 89
              },
           6: {
             episodeNummer: 6,
@@ -67,7 +77,9 @@ const serier = {
             beskrivelse: "En dag så kommer to veldig små gjester til Mummihuset. De har med seg en koffert så Mummipappa forstår de har kommet for å bli og bygger en liten seng til dem. Alle er veldig nysgjerrige på hva som er i kofferten.",
             varighet: "23 min",
             thumbnail: "https://media.themoviedb.org/t/p/w454_and_h254_face/nRFdPRyHtjibDCMtpd6Nboj7vkT.jpg",
-            videoUrl: "https://archive.org/download/folder_202411/I%20Mummidalen/Season%201/I%20Mummidalen%20-%201x06%20-%20Sm%C3%A5%20gjester.mp4"
+            videoUrl: "https://archive.org/download/folder_202411/I%20Mummidalen/Season%201/I%20Mummidalen%20-%201x06%20-%20Sm%C3%A5%20gjester.mp4",
+            introStart: 2,
+            introEnd: 89
           }
         }
       }
