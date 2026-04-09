5680e9eaac16a5cffde2fb44e69f779d6433507b
2000716 <SveinOlavHaukaasArstein@hotmail.com>
2026-04-09 22:34:44 +0200

    Update Seriedata.js

--- Seriedata.js
+++ Seriedata.js
@@ -114,7 +114,7 @@ const serier = {
     tittel: "The Titanic Archive",
     bakgrunn: "https://static.wixstatic.com/media/d9ace9_d2791cd525fb43239a80524ce10b435c~mv2.png",
     logo: "https://static.wixstatic.com/media/d9ace9_a9a200d5078e4797997dc8f8d22a467a~mv2.png",
-    poster: "https://static.wixstatic.com/media/d9ace9_f312e0fdb80c4fd5bc06fd3fdabee8ad~mv2.png",
+    poster: "https://static.wixstatic.com/media/d9ace9_7e0bc76a75f140c28a7e34baca393cdf~mv2.png",
     posterVertikal: "https://static.wixstatic.com/media/d9ace9_f312e0fdb80c4fd5bc06fd3fdabee8ad~mv2.png",
     beskrivelse: "I denne dokumentaren tar Watch Originals med deg hendelsesforløpet til Titanic og historiene til offrene.",
     metadata: ["Norge", "2026", "16", "Dokumentar"],
