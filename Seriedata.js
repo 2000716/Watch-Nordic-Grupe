b55de411713fff913c1e79317aca3d1672648733
2000716 <SveinOlavHaukaasArstein@hotmail.com>
2026-06-12 13:55:47 +0200

    Update Seriedata.js

--- Seriedata.js
+++ Seriedata.js
@@ -11,7 +11,7 @@ const serier = {
     imdb: "",
     skapere: "Watch Nordic Originals Studio",
     tilbakeUrl: "serie.html?navn=the-titanic-archive",
-    publishDate: "2026-05-01T00:00:00", // ISO datoformat
+    publishDate: "2026-05-01T00:00:00",
     expireDate: "2027-11-15T23:59:59",
     audioLanguages: [],
     subtitleLanguages: [],
@@ -23,12 +23,11 @@ const serier = {
           1: {
             episodeNummer: 1,
             tittel: "Titanic",
-            beskrivelse: "Jomfruturen for RMS Titanic er i gang, uten at noen vet at den ender i tragedie. ",
+            beskrivelse: "Jomfruturen for RMS Titanic er i gang, uten at noen vet at den ender i tragedie.",
             varighet: "5 min",
             thumbnail: "https://static.wixstatic.com/media/d9ace9_0d59e15afbca471ba942b417054476a3~mv2.jpg",
             publishDate: "2026-05-20T18:00:00",
-            videoUrl: "https://video.wixstatic.com/video/d9ace9_b70998c45f2f4284b9b764582a0db2f2/1080p/mp4/file.mp4",
-          
+            videoUrl: "https://video.wixstatic.com/video/d9ace9_b70998c45f2f4284b9b764582a0db2f2/1080p/mp4/file.mp4"
           },
           2: {
             episodeNummer: 2,
@@ -55,7 +54,7 @@ const serier = {
         }
       }
     }
-  }, // <-- KOMMA MELLOM SERIER
+  },
 
   "vasaskipet": {
     tittel: "Vasaskipet - Et stille vitne i tiden",
@@ -64,12 +63,12 @@ const serier = {
     logo: "https://static.wixstatic.com/media/d9ace9_7e338923e3c84d92b6a2fdbd1599744b~mv2.png",
     poster: "https://static.wixstatic.com/media/d9ace9_7754297ad1094e16b488b2cbd0b238cf~mv2.jpg",
     posterVertikal: "https://static.wixstatic.com/media/d9ace9_dfe22382394348a1ad40f034d0c8cd82~mv2.jpg",
-    beskrivelse: "I denne dokumentaren tar Watch Originals deg med på reisen til oppdagelsen av krigsskipet Vasa. Hvordan kunne et så mektig krigsskip synke bare få meter fra kaien? Og hvordan har dette skipet blitt et levende vitnesbyrd om hendelsene på 1600-tallet? ",
+    beskrivelse: "I denne dokumentaren tar Watch Originals deg med på reisen til oppdagelsen av krigsskipet Vasa. Hvordan kunne et så mektig krigsskip synke bare få meter fra kaien? Og hvordan har dette skipet blitt et levende vitnesbyrd om hendelsene på 1600-tallet?",
     metadata: ["2026", "10 år", "Dokumentar", "4K"],
     imdb: "",
     skapere: "Watch Nordic Originals Studio",
     tilbakeUrl: "serie.html?navn=vasaskipet",
-    publishDate: "2026-05-02T00:00:00", // ISO datoformat
+    publishDate: "2026-05-02T00:00:00",
     expireDate: "2027-11-15T23:59:59",
     audioLanguages: [],
     subtitleLanguages: [],
@@ -85,8 +84,7 @@ const serier = {
             varighet: "5 min",
             thumbnail: "https://static.wixstatic.com/media/d9ace9_43006d61b876444e829ebf7b67b90d87~mv2.png",
             publishDate: "2026-06-10T18:00:00",
-            videoUrl: "",
-          
+            videoUrl: ""
           },
           2: {
             episodeNummer: 2,
@@ -113,62 +111,62 @@ const serier = {
         }
       }
     }
-   }, // <-- KOMMA MELLOM SERIER
+  },
 
-"apollo11-was-it-real": {
-  tittel: "Apollo 11 - Was It Real?",
-  rating: "0.0",
-  bakgrunn: "https://static.wixstatic.com/media/d9ace9_6189bb0756b8437ab689f6f65652f701~mv2.png",
-  logo: "https://static.wixstatic.com/media/d9ace9_8920f6f7b0b24913a251dde3f208841e~mv2.png",
-  poster: "https://static.wixstatic.com/media/d9ace9_6597c0df53534dfb800580bb6716c27c~mv2.jpg",
-  posterVertikal: "https://static.wixstatic.com/media/d9ace9_bb5d07a805334acaa7917644626f3f56~mv2.jpg",
-  beskrivelse: "I denne dokumentaren tar Watch Originals deg med tilbake til månelandingen med Apollo 11. Men det store spørsmålet mange har stilt seg er: var det ekte?",
-  metadata: ["2026", "Alle", "Dokumentar", "4K"],
-  imdb: "",
-  skapere: "Watch Nordic Originals Studio",
-  tilbakeUrl: "serie.html?navn=apollo11-was-it-real",
-  publishDate: "2026-06-12T00:00:00",
-  expireDate: "2027-11-15T23:59:59",
-  audioLanguages: [],
-  subtitleLanguages: [],
-  sesonger: {
-    1: {
-      sesongNummer: 1,
-      beskrivelse: "Sesong 1",
-      episoder: {
-        1: {
-          episodeNummer: 1,
-          tittel: "Apollo 11",
-          beskrivelse: "Etter mange mislykete forsøk, klarer dem å få Apollo 11 ut i atsmosfæren.",
-          varighet: "5 min",
-          thumbnail: "https://static.wixstatic.com/media/d9ace9_6189bb0756b8437ab689f6f65652f701~mv2.png",
-          publishDate: "2027-06-10T18:00:00",
-          videoUrl: ""
-        },
-        2: {
-          episodeNummer: 2,
-          tittel: "Månelandingen",
-          beskrivelse: "Månelanden er satt i gang, men vil dem komme tilbake?",
-          varighet: "5 min",
-          thumbnail: "https://static.wixstatic.com/media/d9ace9_6189bb0756b8437ab689f6f65652f701~mv2.png",
-          publishDate: "2027-06-10T18:00:00",
-          videoUrl: "",
-          introStart: 2,
-          introEnd: 89
-        },
-        3: {
-          episodeNummer: 3,
-          tittel: "Alt var fake!",
-          beskrivelse: "Teorier og spekulasjoner om at månelandingen er fake øker i avisene.",
-          varighet: "5 min",
-          thumbnail: "https://static.wixstatic.com/media/d9ace9_6189bb0756b8437ab689f6f65652f701~mv2.png",
-          publishDate: "2027-06-10T18:00:00",
-          videoUrl: "",
-          introStart: 2,
-          introEnd: 89
+  "apollo11-was-it-real": {
+    tittel: "Apollo 11 - Was It Real?",
+    rating: "0.0",
+    bakgrunn: "https://static.wixstatic.com/media/d9ace9_6189bb0756b8437ab689f6f65652f701~mv2.png",
+    logo: "https://static.wixstatic.com/media/d9ace9_8920f6f7b0b24913a251dde3f208841e~mv2.png",
+    poster: "https://static.wixstatic.com/media/d9ace9_6597c0df53534dfb800580bb6716c27c~mv2.jpg",
+    posterVertikal: "https://static.wixstatic.com/media/d9ace9_bb5d07a805334acaa7917644626f3f56~mv2.jpg",
+    beskrivelse: "I denne dokumentaren tar Watch Originals deg med tilbake til månelandingen med Apollo 11. Men det store spørsmålet mange har stilt seg er: var det ekte?",
+    metadata: ["2026", "Alle", "Dokumentar", "4K"],
+    imdb: "",
+    skapere: "Watch Nordic Originals Studio",
+    tilbakeUrl: "serie.html?navn=apollo11-was-it-real",
+    publishDate: "2026-06-12T00:00:00",
+    expireDate: "2027-11-15T23:59:59",
+    audioLanguages: [],
+    subtitleLanguages: [],
+    sesonger: {
+      1: {
+        sesongNummer: 1,
+        beskrivelse: "Sesong 1",
+        episoder: {
+          1: {
+            episodeNummer: 1,
+            tittel: "Apollo 11",
+            beskrivelse: "Etter mange mislykete forsøk, klarer dem å få Apollo 11 ut i atmosfæren.",
+            varighet: "5 min",
+            thumbnail: "https://static.wixstatic.com/media/d9ace9_6189bb0756b8437ab689f6f65652f701~mv2.png",
+            publishDate: "2027-06-10T18:00:00",
+            videoUrl: ""
+          },
+          2: {
+            episodeNummer: 2,
+            tittel: "Månelandingen",
+            beskrivelse: "Månelandingen er satt i gang, men vil de komme tilbake?",
+            varighet: "5 min",
+            thumbnail: "https://static.wixstatic.com/media/d9ace9_6189bb0756b8437ab689f6f65652f701~mv2.png",
+            publishDate: "2027-06-10T18:00:00",
+            videoUrl: "",
+            introStart: 2,
+            introEnd: 89
+          },
+          3: {
+            episodeNummer: 3,
+            tittel: "Alt var fake!",
+            beskrivelse: "Teorier og spekulasjoner om at månelandingen er fake øker i avisene.",
+            varighet: "5 min",
+            thumbnail: "https://static.wixstatic.com/media/d9ace9_6189bb0756b8437ab689f6f65652f701~mv2.png",
+            publishDate: "2027-06-10T18:00:00",
+            videoUrl: "",
+            introStart: 2,
+            introEnd: 89
+          }
         }
       }
     }
   }
-  },
-  };
+};
