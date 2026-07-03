92f8fb5784277429e3cd02f037fcf4c753e48c4c
2000716 <SveinOlavHaukaasArstein@hotmail.com>
2026-07-03 19:55:44 +0200

    Update Filmdata.js

--- Filmdata.js
+++ Filmdata.js
@@ -3,36 +3,35 @@ const filmer = {
     tittel: "The Kid",
     rating: "8.2",
     bakgrunn: "https://image.tmdb.org/t/p/original/jnZKUavR4Ju4jTFP3HjGxdgSh9g.jpg",
-    banner: "", 
+    banner: "",
     logo: "https://static.wixstatic.com/media/fd2fb2_bcc26c34a5a2490dabd3f5b6bdf20bb8~mv2.png",
-    trailer: "", // 🎥 NY 
+    trailer: "",
     poster: "https://image.tmdb.org/t/p/original/pMeqhS16RXQhNw2hpBkDvBR7r7Z.jpg",
-    posterVertikal: "https://image.tmdb.org/t/p/original/cIIsmZ4az8AaJJoDgtIOS797RCA.jpg", // portrett
+    posterVertikal: "https://image.tmdb.org/t/p/original/cIIsmZ4az8AaJJoDgtIOS797RCA.jpg",
     beskrivelse: "En mor efterlader sit nyfødte barn i en limousine. På en seddel bønfalder hun ejeren om at tage sig af drengen. Men bilen bliver stjålet at tyve, der smider barnet i skraldespanden.",
     metadata: ["1921", "Alle", "Komedie", "1 t 8 min"],
     skuespillere: "Charles Chaplin, Edna Purviance, Jackie Coogan",
     regissor: "Charles Chaplin",
     watchUrl: "film-mal.html?id=the-kid",
-    videoUrl:"https://dn720708.ca.archive.org/0/items/the-kid-1921_20250723/The%20Kid%20%281921%29.mp4",
-   subtitleUrl: "",
-   tilbakeUrl: "film.html?navn=the-kid",
-  audioLanguages: [
-    { code: "no", label: "Engelsk" } ],
-  subtitleLanguages: [
-    { code: "no", label: "Norsk" },
-    { code: "off", label: "Av" }
-  ],
-   publishDate: "2025-10-02T00:00:00", // ISO datoformat
+    videoUrl: "https://dn720708.ca.archive.org/0/items/the-kid-1921_20250723/The%20Kid%20%281921%29.mp4",
+    subtitleUrl: "",
+    tilbakeUrl: "film.html?navn=the-kid",
+    audioLanguages: [{ code: "en", label: "Engelsk" }],
+    subtitleLanguages: [
+      { code: "nb", label: "Norsk" },
+      { code: "off", label: "Av" }
+    ],
+    publishDate: "2025-10-02T00:00:00",
     expireDate: "2027-11-15T23:59:59",
   },
 
-"house-on-haunted-hill": {
+  "house-on-haunted-hill": {
     tittel: "House on Haunted Hill",
     rating: "6.7",
     bakgrunn: "https://image.tmdb.org/t/p/original/tS2qaOAG1Yqepp0Hm9AJjH7Zg14.jpg",
-    banner: "", 
+    banner: "",
     logo: "https://image.tmdb.org/t/p/original/ulRnCQUtXRLNnQKu4vRmMAg0llS.png",
-    trailer: "", 
+    trailer: "",
     poster: "https://image.tmdb.org/t/p/original/9S97udaoo9wP5puoeyGlHK4I8iN.jpg",
     posterVertikal: "https://image.tmdb.org/t/p/original/p0Gh3QubR6WPhfPkKJXuYO76zag.jpg",
     beskrivelse: "En millionær tilbyr 10 000 dollar til fem personer som går med på å være innelåst i et stort, skummelt, leid hus over natten med ham og kona.",
@@ -40,52 +39,53 @@ const filmer = {
     skuespillere: "Vincent Price, Carol Ohmart, Richard Long",
     regissor: "William Castle",
     watchUrl: "film-mal.html?id=house-on-haunted-hill",
-    videoUrl:"https://ia801504.us.archive.org/20/items/house-on-haunted-hill-1959_202511/House%20on%20Haunted%20Hill%20%281959%29/House%20on%20Haunted%20Hill%20-%201959.mp4",
+    videoUrl: "https://ia801504.us.archive.org/20/items/house-on-haunted-hill-1959_202511/House%20on%20Haunted%20Hill%20%281959%29/House%20on%20Haunted%20Hill%20-%201959.mp4",
     subtitleUrl: "house-on-haunted-hill.VTT",
     tilbakeUrl: "film.html?navn=house-on-haunted-hill",
-    audioLanguages: [
-      { code: "no", label: "Engelsk" } // Endret også code her fra "no" til "en" siden det er engelsk tale
-    ],
+    audioLanguages: [{ code: "en", label: "Engelsk" }],
     subtitleLanguages: [
-      { code: "no", label: "Norsk" }, // Endret fra "no" til "nb"
+      { code: "nb", label: "Norsk" },
       { code: "off", label: "Av" }
     ],
     publishDate: "2025-10-02T00:00:00",
     expireDate: "2027-11-15T23:59:59",
   },
 
-      "haxan": {
-    tittel: "Häxan ",
+  "haxan": {
+    tittel: "Häxan",
     rating: "7.6",
     bakgrunn: "https://image.tmdb.org/t/p/original/gZwQip2KQRDNZZOEDe0Yi25tW9Z.jpg",
-    banner: "", 
-    logo: "https://beam-images.warnermediacdn.com/BEAM_LWM_DELIVERABLES/730f0be0-7cf9-48ee-86a5-0b1352a85a1d/c3126a93065c93330d8a8e8a440bd604b9d998da.png?submitted=1684661760193&host=wbd-images.prod-vod.h264.io&partner=beamcom&w=4320",
-    trailer: "", // 🎥 NY 
+    banner: "",
+    logo: "https://beam-images.warnermediacdn.com/BEAM_LWM_DELIVERABLES/730f0be0-7cf9-48ee-86a5-0b1352a85a1d/c3126a93065c93330d8a8e8a440bd604b9d998da.png",
+    trailer: "",
     poster: "https://image.tmdb.org/t/p/original/5TqIPddZaDsza8ID7X5HGwX2Txv.jpg",
-    posterVertikal: "https://image.tmdb.org/t/p/original/A4ErFUf7FGNF3DFjp0CaRchxeNL.jpg", // portrett
+    posterVertikal: "https://image.tmdb.org/t/p/original/A4ErFUf7FGNF3DFjp0CaRchxeNL.jpg",
     beskrivelse: "Fiksjonalisert dokumentar som viser utviklingen av hekseri, fra dens hedenske røtter til forvirring med hysteri i Øst-Europa.",
     metadata: ["1922", "15 år", "Grøsser", "1 t 45 min"],
     skuespillere: "Benjamin Christensen, Elisabeth Christensen, Maren Pedersen",
     regissor: "Benjamin Christensen",
     watchUrl: "film-mal.html?id=haxan",
-    videoUrl:"https://dn710205.ca.archive.org/0/items/haxan-1922_202312/H%C3%A4xan%20%281922%29.mp4",
-   subtitleUrl: "",
-   tilbakeUrl: "film.html?navn=haxan",
-  audioLanguages: [    { code: "no", label: "Svensk" } 
-  ],
-   publishDate: "2025-10-02T00:00:00", // ISO datoformat
+    videoUrl: "https://dn710205.ca.archive.org/0/items/haxan-1922_202312/H%C3%A4xan%20%281922%29.mp4",
+    subtitleUrl: "",
+    tilbakeUrl: "film.html?navn=haxan",
+    audioLanguages: [{ code: "sv", label: "Svensk" }],
+    subtitleLanguages: [
+      { code: "nb", label: "Norsk" },
+      { code: "off", label: "Av" }
+    ],
+    publishDate: "2025-10-02T00:00:00",
     expireDate: "2027-11-15T23:59:59",
   },
 
-       "sprite-fright": {
+  "sprite-fright": {
     tittel: "Sprite Fright",
     rating: "7.5",
     bakgrunn: "https://image.tmdb.org/t/p/original/aXZuvuQpcXivXHasXFAkOhfTTfb.jpg",
-    banner: "", 
+    banner: "",
     logo: "",
-    trailer: "", // 🎥 NY 
+    trailer: "",
     poster: "https://static.wixstatic.com/media/d9ace9_af1093b9b682484987fc8f30d24dd3f5~mv2.jpg",
-    posterVertikal: "https://pics.filmaffinity.com/sprite_fright-557419042-large.jpg", // portrett
+    posterVertikal: "https://pics.filmaffinity.com/sprite_fright-557419042-large.jpg",
     beskrivelse: "Når en gruppe bråkete tenåringer drar inn i en isolert skog, oppdager de fredelige soppvesener som viser seg å være en uventet naturkraft.",
     metadata: ["2021", "15 år", "Animasjon", "10 min"],
     skuespillere: "Valerie LaPointe, Gabriel Bisset-Smith, Nicole Davis",
@@ -93,79 +93,77 @@ const filmer = {
     lisens: "(CC) Blender Foundation | studio.blender.org",
     kilde: "https://www.blender.org/about/license/",
     watchUrl: "film-mal.html?id=sprite-fright",
-    videoUrl:"https://dn721904.ca.archive.org/0/items/sprite-fright-blender-open-movie/Sprite%20Fright%20-%20Blender%20Open%20Movie.mp4",
-   subtitleUrl: "",
-   tilbakeUrl: "film.html?navn=sprite-fright",
-  audioLanguages: [
-    { code: "no", label: "Engelsk" } ],
-  subtitleLanguages: [
-    { code: "no", label: "Norsk" },
-    { code: "off", label: "Av" }
-  ],
-   publishDate: "2026-01-01T00:00:00", // ISO datoformat
+    videoUrl: "https://dn721904.ca.archive.org/0/items/sprite-fright-blender-open-movie/Sprite%20Fright%20-%20Blender%20Open%20Movie.mp4",
+    subtitleUrl: "",
+    tilbakeUrl: "film.html?navn=sprite-fright",
+    audioLanguages: [{ code: "en", label: "Engelsk" }],
+    subtitleLanguages: [
+      { code: "nb", label: "Norsk" },
+      { code: "off", label: "Av" }
+    ],
+    publishDate: "2026-01-01T00:00:00",
     expireDate: "2029-11-15T23:59:59",
   },
 
-         "wing-it": {
+  "wing-it": {
     tittel: "Wing It! - A Pet Project",
     rating: "6.9",
     bakgrunn: "https://static.wixstatic.com/media/d9ace9_c4277ef5983b479c839d1e8f3673d4cf~mv2.jpg",
-    banner: "", 
+    banner: "",
     logo: "",
-    trailer: "", // 🎥 NY 
+    trailer: "",
     poster: "https://www.blendernation.com/wp-content/uploads/2023/09/4c0c5ec3c2380059942395be8570912c.jpg",
-    posterVertikal: "https://studio.blender.org/files/cache/04/61/046118290346d04eee9444bb5aa0e80c.jpg", // portrett
+    posterVertikal: "https://studio.blender.org/files/cache/04/61/046118290346d04eee9444bb5aa0e80c.jpg",
     beskrivelse: "En anspent ingeniør får uvelkomment besøk av en entusiastisk wannabe-pilot, noe som fører til at begge blir skutt opp i luften i en romferge som er ute av kontroll.",
     metadata: ["2023", "Alle", "Animasjon", "4 min"],
     regissor: "Rik Schutte",
     lisens: "(CC) Blender Foundation | studio.blender.org",
     kilde: "https://studio.blender.org/projects/wing-it/pages/licensing/",
     watchUrl: "film-mal.html?id=wing-it",
-    videoUrl:"https://video.wixstatic.com/video/d9ace9_3e29721f57c84b2f9652b0f1b7185a26/1080p/mp4/file.mp4",
-   subtitleUrl: "",
-   tilbakeUrl: "film.html?navn=wing-it",
-  audioLanguages: [
-    { code: "no", label: "Engelsk" } ],
-  subtitleLanguages: [
-    { code: "no", label: "Norsk" },
-    { code: "off", label: "Av" }
-  ],
-   publishDate: "2026-04-30T00:00:00", // ISO datoformat
+    videoUrl: "https://video.wixstatic.com/video/d9ace9_3e29721f57c84b2f9652b0f1b7185a26/1080p/mp4/file.mp4",
+    subtitleUrl: "",
+    tilbakeUrl: "film.html?navn=wing-it",
+    audioLanguages: [{ code: "en", label: "Engelsk" }],
+    subtitleLanguages: [
+      { code: "nb", label: "Norsk" },
+      { code: "off", label: "Av" }
+    ],
+    publishDate: "2026-04-30T00:00:00",
     expireDate: "2027-01-10T23:59:59",
   },
 
-    "singularity": {
+  "singularity": {
     tittel: "Singularity",
     rating: "0.0",
     bakgrunn: "https://studio.blender.org/static/common/images/project-detail/singularity/singularity-story-concept-020_0030.0f6b6798532d.webp",
-    banner: "", 
+    banner: "",
     logo: "",
-    trailer: "", // 🎥 NY 
+    trailer: "",
     poster: "https://i.ytimg.com/vi/l5OZu-IrXpw/maxresdefault.jpg",
-    posterVertikal: "https://image.tmdb.org/t/p/original/tsTGGB6NGVzHMuNXEV328jU0Eyx.jpg", // portrett
+    posterVertikal: "https://image.tmdb.org/t/p/original/tsTGGB6NGVzHMuNXEV328jU0Eyx.jpg",
     beskrivelse: "Når et illevarslende sort hull sluker det iskalde hjemmet til et lite romvesen, må det finne tilflukt blant en gruppe lysende skapninger som overvåkes av et mystisk himmelvesen.",
     metadata: ["2026", "Alle", "Animasjon", "7 min"],
     regissor: "Andy Goralczyk",
     lisens: "(CC) Blender Foundation | studio.blender.org",
     kilde: "https://studio.blender.org/projects/singularity/pages/licensing/",
     watchUrl: "film-mal.html?id=singularity",
-    videoUrl:"https://video.wixstatic.com/video/d9ace9_da9f9d0843644bb6a5efdaea6852f7b9/360p/mp4/file.mp4",
-   subtitleUrl: "",
-   tilbakeUrl: "film.html?navn=singularity",
-  audioLanguages: [
-  ],
-   publishDate: "2026-05-11T00:00:00", // ISO datoformat
+    videoUrl: "https://video.wixstatic.com/video/d9ace9_da9f9d0843644bb6a5efdaea6852f7b9/360p/mp4/file.mp4",
+    subtitleUrl: "",
+    tilbakeUrl: "film.html?navn=singularity",
+    audioLanguages: [],
+    subtitleLanguages: [],
+    publishDate: "2026-05-11T00:00:00",
     expireDate: "2027-01-10T23:59:59",
   },
 
-    "tears-of-steel": {
+  "tears-of-steel": {
     tittel: "Tears of Steel",
     rating: "5.5",
     bakgrunn: "https://m.media-amazon.com/images/M/MV5BODc0MDgzZDAtOWM5YS00ZTg0LTlhMDEtMjljNmJiZDg5MmExXkEyXkFqcGc@._V1_.jpg",
-    banner: "https://mango.blender.org/wp-content/uploads/2013/05/08_captain_thom.jpg", 
-    bannerMobil: "https://mango.blender.org/wp-content/uploads/2013/05/08_captain_thom.jpg",  
+    banner: "https://mango.blender.org/wp-content/uploads/2013/05/08_captain_thom.jpg",
+    bannerMobil: "https://mango.blender.org/wp-content/uploads/2013/05/08_captain_thom.jpg",
     logo: "",
-    trailer: "", 
+    trailer: "",
     poster: "https://m.media-amazon.com/images/S/pv-target-images/ac878c08d94a3da94048c54c4585013f2c7672c33ef5236307eba9fb8ae96d38.png",
     posterVertikal: "https://image.tmdb.org/t/p/original/8qy3jRmaHR7f8VZh3iXCqCWfFsH.jpg",
     beskrivelse: "I et dystopisk, fremtidig Amsterdam må en gruppe forskere bruke avansert teknologi for å redesigne fortiden og redde verden fra en altetende hær av destruktive roboter.",
@@ -178,47 +176,12 @@ const filmer = {
     videoUrl: "https://dn710604.ca.archive.org/0/items/tearsofsteelblendervfxopenmovie800p/Tears%20of%20Steel%20-%20Blender%20VFX%20Open%20Movie%20800p.mp4",
     subtitleUrl: "tears-of-Steel.VTT",
     tilbakeUrl: "film.html?navn=tears-of-steel",
-    audioLanguages: [
-    { code: "no", label: "Engelsk" } ],
+    audioLanguages: [{ code: "en", label: "Engelsk" }],
     subtitleLanguages: [
-    { code: "no", label: "Norsk" },
-    { code: "off", label: "Av" }
-  ],
+      { code: "nb", label: "Norsk" },
+      { code: "off", label: "Av" }
+    ],
     publishDate: "2026-06-25T00:00:00",
     expireDate: "2030-01-01T23:59:59",
   },
-  };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+};
