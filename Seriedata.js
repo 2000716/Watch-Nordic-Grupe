783b8f23f41e4a0ca2abff4f60e2f0dd0779baf4
2000716 <SveinOlavHaukaasArstein@hotmail.com>
2026-07-03 20:58:24 +0200

    Update Seriedata.js

--- Seriedata.js
+++ Seriedata.js
@@ -3,6 +3,7 @@ const serier = {
     tittel: "The Titanic Archive",
     rating: "0.0",
     bakgrunn: "https://static.wixstatic.com/media/d9ace9_3d752a11a2d04c3fb9d02873d4cb659a~mv2.png",
+    banner: "https://static.wixstatic.com/media/d9ace9_6189bb0756b8437ab689f6f65652f701~mv2.png", 
     logo: "https://static.wixstatic.com/media/d9ace9_a9a200d5078e4797997dc8f8d22a467a~mv2.png",
     poster: "https://static.wixstatic.com/media/d9ace9_2d7b7487a34b4d09bb824dd07baa47a1~mv2.png",
     posterVertikal: "https://static.wixstatic.com/media/d9ace9_f312e0fdb80c4fd5bc06fd3fdabee8ad~mv2.png",
