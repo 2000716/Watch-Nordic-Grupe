58b91ce9238bd870b70d491e1f759669ec309547
2000716 <SveinOlavHaukaasArstein@hotmail.com>
2026-07-03 15:08:06 +0200

    Update firebase-oppsett.js

--- firebase-oppsett.js
+++ firebase-oppsett.js
@@ -1,9 +1,11 @@
 // firebase-oppsett.js
 // ALT ER OPPDATERT TIL VERSJON 12.15.0 FOR Å MATCHE PERFEKT
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
+// Din unike Firebase-konfigurasjon
 const firebaseConfig = {
   apiKey: "AIzaSyBlfCbB1AuiKVHMBEhYd0cvkJ0jxHVZfUg",
   authDomain: "watch-nordic-78b99.firebaseapp.com",
@@ -13,8 +15,9 @@ const firebaseConfig = {
   appId: "1:541804766412:web:83fc77721e384131a1ce69"
 };
 
+// Initialiser Firebase-applikasjonen
 const app = initializeApp(firebaseConfig);
 
-// Eksporterer auth og db til de andre sidene dine
+// Eksporterer auth og db til de andre sidene og skriptene dine (f.eks. videospilleren)
 export const auth = getAuth(app);
 export const db = getFirestore(app);
