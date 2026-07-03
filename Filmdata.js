24f0c041a3b16acf9ce3e0e0d2ed98e60575d583
2000716 <SveinOlavHaukaasArstein@hotmail.com>
2026-07-03 21:15:37 +0200

    Update Filmdata.js

--- Filmdata.js
+++ Filmdata.js
@@ -160,7 +160,6 @@ const filmer = {
     tittel: "Tears of Steel",
     rating: "5.5",
     bakgrunn: "https://m.media-amazon.com/images/M/MV5BODc0MDgzZDAtOWM5YS00ZTg0LTlhMDEtMjljNmJiZDg5MmExXkEyXkFqcGc@._V1_.jpg",
-    banner: "",
     bannerMobil: "https://mango.blender.org/wp-content/uploads/2013/05/08_captain_thom.jpg",
     logo: "",
     trailer: "",
