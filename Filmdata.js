73e086476196d3cf6f004b9f8e2a9c8bec3bf921
2000716 <SveinOlavHaukaasArstein@hotmail.com>
2026-07-31 11:10:31 +0200

    Update Filmdata.js

--- Filmdata.js
+++ Filmdata.js
@@ -163,8 +163,8 @@ const filmer = {
     expireDate: "2030-01-01T23:59:59",
   },
 
-    "helloween-spesial-detectiv-muffin": {
-    tittel: "Detektiv Muffin: Halloween spesial",
+"halloween-spesial-detective-muffin": {
+    tittel: "Detektiv Muffin: Halloween-spesial",
     rating: "0.0",
     bakgrunn: "https://static.wixstatic.com/media/d9ace9_a82288441b2c4981b162e76d1458ea26~mv2.jpg",
     bakgrunnMobil: "https://static.wixstatic.com/media/d9ace9_4148dac16a81424e9a08e544106c05e4~mv2.jpg",
@@ -176,19 +176,19 @@ const filmer = {
     beskrivelse: "Bli med Detektiv Muffins og Herr Marshmallow når en koselig kveld foran skjermen raskt forvandles til et ekte Halloween-mysterium. Klarer de å finne ut hva som skjuler seg i skyggene før filmen er over?",
     metadata: ["2026", "Alle", "Barn", "5 min"],
     skuespillere: "",
-    regissor: "Watch Nordic Originals Studio",
+    regissør: "Watch Nordic Originals Studio",
     lisens: "",
     kilde: "",
-    watchUrl: "film-mal.html?id=helloween-spesial-detectiv-muffin",
+    watchUrl: "film-mal.html?id=halloween-spesial-detective-muffin",
     videoUrl: "",
     subtitleUrl: "",
-    tilbakeUrl: "film.html?navn=helloween-spesial-detectiv-muffin",
-    audioLanguages: [{ code: "en", label: "Norsk" }],
+    tilbakeUrl: "film.html?navn=halloween-spesial-detective-muffin",
+    audioLanguages: [{ code: "no", label: "Norsk" }],
     subtitleLanguages: [
       { code: "no", label: "Norsk" },
       { code: "off", label: "Av" }
     ],
     publishDate: "2026-10-23T00:00:00",
-    expireDate: "2030-01-01T23:59:59",
+    expireDate: "2030-01-01T23:59:59"
   },
 };
