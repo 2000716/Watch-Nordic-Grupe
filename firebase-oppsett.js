f30ecda9b466f3ed93a52b0a6ee759413bffb962
2000716 <SveinOlavHaukaasArstein@hotmail.com>
2026-09-06 00:48:18 +0200

    Refactor Firebase setup and update API key

--- firebase-oppsett.js
+++ firebase-oppsett.js
@@ -1,14 +1,20 @@
 // firebase-oppsett.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-import { getAuth, setPersistence, browserLocalPersistence, inMemoryPersistence } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+import { 
+  getAuth, 
+  setPersistence, 
+  browserLocalPersistence, 
+  inMemoryPersistence 
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
 import { 
   initializeFirestore, 
   persistentLocalCache, 
   persistentMultipleTabManager 
 } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
+// Firebase-konfigurasjon
 const firebaseConfig = {
-  apiKey: "AIzaSyBlfCbB1AuiKVHMBEhYd0cvkJ0jxHVZfUg",
+  apiKey: "DIN_NYE_API_NØKKEL_HER", // Lim inn den nye API-nøkkelen fra Google Cloud Console her
   authDomain: "watch-nordic-78b99.firebaseapp.com",
   projectId: "watch-nordic-78b99",
   storageBucket: "watch-nordic-78b99.firebasestorage.app",
@@ -16,18 +22,20 @@ const firebaseConfig = {
   appId: "1:541804766412:web:83fc77721e384131a1ce69"
 };
 
-// Initialiser Firebase
+// Initialiser Firebase App
 const app = initializeApp(firebaseConfig);
+
+// Initialiser Autentisering
 export const auth = getAuth(app);
 
-// Sikrere håndtering av persistens (faller tilbake til minne i stedet for å krasje i privat modus)
+// Håndtering av innloggingstilstand (persistens)
 setPersistence(auth, browserLocalPersistence).catch(() => {
   setPersistence(auth, inMemoryPersistence).catch((err) => {
     console.warn("Kunne ikke sette innloggingspersistens:", err);
   });
 });
 
-// Trygg initialisering av Firestore
+// Trygg initialisering av Firestore med lokal hurtigbuffer (cache)
 let firestoreDb;
 try {
   firestoreDb = initializeFirestore(app, {
@@ -43,4 +51,5 @@ try {
   });
 }
 
+// Eksporter Firestore-databasen
 export const db = firestoreDb;
